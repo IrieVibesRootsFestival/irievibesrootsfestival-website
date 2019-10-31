f55c496570d5aed4ef2d74860bf605fd58ce3bb1
Anna <djsndcx@gmail.com>
2019-10-31 10:51:59 +0100

    Desotec pages
    Added desotec content and images

--- fr/info/eco/clean-water/index.php
+++ fr/info/eco/clean-water/index.php
@@ -10,7 +10,20 @@
         <?php include($_SERVER['DOCUMENT_ROOT'].'/src/static/pages/header_fr.php');?>
         <div class="container content">         
             <h1 style="margin-bottom:5px" class="display-7 text-center">Eau recyclée</h1>
+            <div class="row">
+                <div class="col-12 col-md-3 order-md-2"><img class="img-fluid rounded" src="/src/img/partners/desotec.jpg"/></div>
+                <div class="col-12 col-md-9">
+                    <p>Vous savez que…</p>
+                    <ul>
+                        <li>grace au remplacement des toilettes et au filtrage de nos eaux en collaboration avec <a href="http://www.desotec.com/">Desotec Activated Carbon</a>, nous avons réussi à réduire de moitié notre consommation d’eau pendant le festival?</li>
+                        <li>chaque goutte d’eau est purifiée et réutilisée 5 fois pendant le festival?</li>
+                    </ul>
+                </div>
+                <div class="col-auto"><img class="img-fluid rounded" src="/src/img/eco/clean-water-advertisement.png"/></div>
+                <div class="col-auto"><img class="img-fluid rounded" src="/src/img/eco/clean-water-desotec-filter.jpg"/></div>
+                <div class="col-auto"><img class="img-fluid rounded" src="/src/img/eco/clean-water-system.jpg"/></div>
             </div>
+        </div>
         <!-- Include Footer -->
         <?php include($_SERVER['DOCUMENT_ROOT'].'/src/static/pages/footer_fr.php');?>        
     </body>
