f4dc7376e3cc29b183d64d1deadd4e1c3deef3a0
Anna <djsndcx@gmail.com>
2019-10-31 12:47:00 +0100

    Mooimakers & Cleanup Day
    Testing a cleanup day photo layout

--- nl/info/eco/eco-team/index.php
+++ nl/info/eco/eco-team/index.php
@@ -10,6 +10,32 @@
         <?php include($_SERVER['DOCUMENT_ROOT'].'/src/static/pages/header_nl.php');?>
         <div class="container content">         
             <h1 style="margin-bottom:5px" class="display-7 text-center">Eco Team</h1>
+            <br/>
+            <div class="row">
+                <div class="col-12 col-md-3 order-md-2"><img class="img-fluid rounded" src="/src/img/partners/mooimakers.jpg"/></div>
+                <div class="col-12 col-md-9">
+                    <p>Wist je dat…</p>
+                    <ul>
+                        <li>wij ook gedurende het jaar in de weer zijn voor een betere en propere wereld?</li>
+                        <li>wij deelnemen aan de World Cleanup Day?</li>
+                        <li>ons ecoteam gedurende het festival voortdurend aan de slag is om de terreinen proper te houden ?</li>
+                        <li>wij gratis zakasbakjes ter beschikking stellen van onze bezoekers ?</li>
+                        <li>wij voor de catering opbouw/afbraak én backstage vooral oog hebben voor food waste en hiervoor samen met lokale handelaren een samenwerking op touw hebben gezet?</li>
+                        <li>je als bezoeker ook een bezoekje kan brengen aan de compostmobiel?</li>
+                    </ul>
+                </div>
+            </div>
+            <!-- Mooimakers Pictures -->
+            <div class="row">
+                <div class="col-12 col-md-4"><img class="img-fluid rounded" src="/src/img/eco/mooimakers/ttf-mooimaker.jpg"/></div>
+                <div class="col-12 col-md-4"><img class="img-fluid rounded" src="/src/img/eco/mooimakers/cleanup-day-head.jpg"></div>
+                
+                <div class="col-6 col-md-4"><img class="img-fluid rounded" src="/src/img/eco/mooimakers/cleanup-day-0.jpg"/></div>
+                <div class="col-6 col-md-4"><img class="img-fluid rounded" src="/src/img/eco/mooimakers/cleanup-day-2.jpg"/></div>
+                <div class="col-12 col-md-4"><img class="img-fluid rounded" src="/src/img/eco/mooimakers/cleanup-day-4.jpg"/></div>
+                <div class="col-6 col-md-6"><img class="img-fluid rounded" src="/src/img/eco/mooimakers/cleanup-day-1.jpg"/></div>
+                <div class="col-6 col-md-6"><img class="img-fluid rounded" src="/src/img/eco/mooimakers/cleanup-day-3.jpg"/></div>
+            </div>
             </div>
         <!-- Include Footer -->
         <?php include($_SERVER['DOCUMENT_ROOT'].'/src/static/pages/footer_nl.php');?>        
