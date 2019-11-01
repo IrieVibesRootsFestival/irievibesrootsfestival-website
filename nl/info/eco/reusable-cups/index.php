8d49ddfbf20d87082655e7311426b76675366d4d
Anna <djsndcx@gmail.com>
2019-11-01 09:27:02 +0100

    Add cups page

--- nl/info/eco/reusable-cups/index.php
+++ nl/info/eco/reusable-cups/index.php
@@ -10,7 +10,28 @@
         <?php include($_SERVER['DOCUMENT_ROOT'].'/src/static/pages/header_nl.php');?>
         <div class="container content">         
             <h1 style="margin-bottom:5px" class="display-7 text-center">Herbruikbare bekers</h1>
+            <br/>
+            <div class="row">
+                <div class="col-12 col-md-9">
+                    <p>Wist je dat...</p>
+                    <ul>
+                        <li>wij al sinds de beginjaren enkel en alleen gebruik maken van herbruikbare bekers?</li>
+                        <li>onze rietjes volledig biologisch afbreekbaar zijn?</li>
+                        <li>onze munten gemaakt zijn van zetmeel gewonnen uit aardappelschillen en de inkt volledig biologisch afbreekbaar is binnen een jaar?</li>
+                        <li>je tijdens het festival onze bekers "leent"? Je betaalt een waarborg voor je eerste beker en bij inlevering geven wij jou het geld terug.</li>
+                        <li>je de bekers kan inruilen aan het bonnetjes kot, maar ook op zondag aan de camping bar?</li>
+                    </ul>
+                </div>
+                <div class="col-12 col-md-3 text-center"><img class="img-fluid rounded" src="/src/img/eco/cups-main.jpg"/>
+                    <small>onze eigen beker met logo</small>
+                </div>
             </div>
+            <!-- Pictures -->
+            <div class="row">
+                <div class="col-12 col-md-6 p-1"><img class="img-fluid rounded" src="/src/img/eco/cups-0.jpg"/></div>
+                <div class="col-12 col-md-6 p-1"><img class="img-fluid rounded" src="/src/img/eco/cups-1.jpg"/></div>
+            </div>
+        </div>
         <!-- Include Footer -->
         <?php include($_SERVER['DOCUMENT_ROOT'].'/src/static/pages/footer_nl.php');?>        
     </body>
