4e075473bd6bdeb037b0376afbbc911ff698079e
Anna <djsndcx@gmail.com>
2019-11-04 00:10:32 +0100

    Trying out new Contact page

--- en/contact/index.php
+++ en/contact/index.php
@@ -2,133 +2,102 @@
     <head>
         <!-- Import meta tags, OpenGraphs, stylesheets and common includes -->
         <?php include($_SERVER['DOCUMENT_ROOT'].'/src/static/pages/head_common.php');?>
-        <title>Home - Irie Vibes</title>
-        <meta property="og:title" content="Home"/>
-        <meta property="og:description" content="Back in time to where it all began, Irie Vibes invites you to their Back to The Roots edition during the heart of summer 2020 in Kortemark." />
+        <title>Contact - Irie Vibes</title>
+        <meta property="og:title" content="Our contact info"/>
+        <meta property="og:description" content="Contact Irie Vibes" />
     </head>
     <body>
         <?php include($_SERVER['DOCUMENT_ROOT'].'/src/static/pages/header_en.php');?>
         <div class="content container">
-            <br/>
             <h3 class="display-7 text-center">Contact</h3>
-            <div id="contact" class="card-deck-wrapper">
-                <div class="card-deck">
-                    <div class="card">                        
-                        <div class="card-body">
-                            <h5 class="card-title">Security</h5>
-                            <p class="card-text">For issues and/or questions regarding security</p>
-                            <small class="card-text text-muted"></small>
-                        </div>
-                        <div class="card-footer">
-                            <p class="text-muted"><i class="fas fa-envelope text-muted"></i><a href="mailto:security@irievibes.be"> security@irievibes.be</a></p>
-                        </div>
-                    </div>                    
-                    <div class="card">                        
-                        <div class="card-body">
-                            <h5 class="card-title">Guests & Volunteers</h5>
-                            <p class="card-text">Questions about volunteership? Ask them here!</p>
-                            <small class="card-text text-muted">Cindy Jonckheere</small>
-                        </div>
-                        <div class="card-footer">
-                            <p class="text-muted"><i class="fas fa-envelope text-muted"></i><a href="mailto:volunteers@irievibes.be"> volunteers@irievibes.be</a></p>
-                        </div>
-                    </div>
-                    <div class="card">                        
-                        <div class="card-body">
-                            <h5 class="card-title">Artists</h5>
-                            <p class="card-text">Interested in coming to Irie Vibes events as an artist?</p>
-                            <small class="card-text text-muted">Davy Landuyt <span class="color-mainstage">(Mainstage)</span></small><br/>
-                            <small class="card-text text-muted">Sam Ducasteele <span class="color-dub-castle">(Dub Castle)</span></small>
-                        </div>
-                        <div class="card-footer">
-                            <p class="text-muted"><i class="fas fa-envelope text-muted"></i><a href="mailto:artist@irievibes.be"> artist@irievibes.be</a></p>
-                        </div>
-                    </div>
-                    <div class="card">                        
-                        <div class="card-body">
-                            <h5 class="card-title">Shops & Foodstands</h5>
-                            <p class="card-text">For questions about obtaining a parcel for your stand.</p>
-                            <small class="card-text text-muted">Ofelie Callewaert</small>
-                        </div>
-                        <div class="card-footer">
-                            <p class="text-muted"><i class="fas fa-envelope text-muted"></i><a href="mailto:shops@irievibes.be"> shops@irievibes.be</a></p>
+            <br/>
+            <div class="row text-irie">
+                <div class="col-12 col-md-4 col-lg-3">
+                    <div class="text-box-2 rounded p-3">
+                        <div class="text-box rounded p-2">
+                            <h4>Security</h4>
+                            <hr/>
+                            <p>For issues and/or questions regarding security</p>
+                            <i class="fas fa-envelope text-muted"></i><a href="mailto:security@irievibes.be"> security@irievibes.be</a>
                         </div>
                     </div>
                 </div>
-                <div class="card-deck">
-                    <div class="card">                        
-                        <div class="card-body">
-                            <h5 class="card-title">Finance</h5>
-                            <p class="card-text">In case you question our tax pays.</p>
-                            <small class="card-text text-muted">Ofélie Callewaert</small>
-                        </div>
-                        <div class="card-footer">
-                            <p class="text-muted"><i class="fas fa-envelope text-muted"></i><a href="mailto:finance@irievibes.be"> finance@irievibes.be</a></p>
+                <div class="col-12 col-md-4 col-lg-3">
+                    <div class="text-box-2 rounded p-3">
+                        <div class="text-box rounded p-2">
+                            <h4>Guests & Volunteers</h4>
+                            <hr/>
+                            <p>Questions about volunteership? Ask them here!</p>
+                            <i class="fas fa-envelope text-muted"></i><a href="mailto:volunteers@irievibes.be"> volunteers@irievibes.be</a>
                         </div>
                     </div>
-                    <div class="card">                        
-                        <div class="card-body">
-                            <h5 class="card-title">Promotions</h5>
-                            <p class="card-text">Want to promote your business or provide sponsorship?</p>
-                            <small class="card-text text-muted">Lalita Vandenweghe<br/>Kimberley Algoedt</small>
-                        </div>
-                        <div class="card-footer">
-                            <p class="text-muted"><i class="fas fa-envelope text-muted"></i><a href="mailto:promo@irievibes.be"> promo@irievibes.be</a></p>
-                        </div>
-                    </div>
-                    <div class="card">                        
-                        <div class="card-body">
-                            <h5 class="card-title">Tech</h5>
-                            <p class="card-text">For all your inquiries about our tech-magic.</p>
-                            <small class="card-text text-muted">Cindy Jonckheere</small>
-                        </div>
-                        <div class="card-footer">
-                            <p class="text-muted"><i class="fas fa-envelope text-muted"></i><a href="mailto:info@irievibes.be"> info@irievibes.be</a></p>
+                </div>
+                <div class="col-12 col-md-4 col-lg-3">
+                    <div class="text-box-2 rounded p-3">
+                        <div class="text-box rounded p-2">
+                            <h4>Artists</h4>
+                            <hr/>
+                            <p>Interested in coming to Irie Vibes events as an artist?</p>
+                            <i class="fas fa-envelope text-muted"></i><a href="mailto:artist@irievibes.be"> artist@irievibes.be</a>
                         </div>
                     </div>
-                    <div class="card">                        
-                        <div class="card-body">
-                            <h5 class="card-title">More Info</h5>
-                            <p class="card-text">For general questions that do not have a specific address</p>
-                            <small class="card-text text-muted">Lalita Vandenweghe<br/>Cindy Jonckheere</small>
-                        </div>
-                        <div class="card-footer">
-                            <p class="text-muted"><i class="fas fa-envelope text-muted"></i><a href="mailto:info@irievibes.be">info@irievibes.be</a></p>
+                </div>
+                <div class="col-12 col-md-4 col-lg-3">
+                    <div class="text-box-2 rounded p-3">
+                        <div class="text-box rounded p-2">
+                            <h4>Shops & Foodstands</h4>
+                            <hr/>
+                            <p>For questions about obtaining a parcel for your stand.</p>
+                            <i class="fas fa-envelope text-muted"></i><a href="mailto:shops@irievibes.be"> shops@irievibes.be</a>
                         </div>
                     </div>
-                </div>                
-                <p class="text-center">Found a bug in the website? Contact the webmaster: <a href="mailto:web@irievibes.be">web@irievibes.be</a>.</p>                
+                </div>
+                <div class="col-12 col-md-4 col-lg-3">
+                    <div class="text-box-2 rounded p-3">
+                        <div class="text-box rounded p-2">
+                            <h4>Finance</h4>
+                            <hr/>
+                            <p>In case you question our tax pays.</p>
+                            <i class="fas fa-envelope text-muted"></i><a href="mailto:finance@irievibes.be"> finance@irievibes.be</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-md-4 col-lg-3">
+                    <div class="text-box-2 rounded p-3">
+                        <div class="text-box rounded p-2">
+                            <h4>Promotions</h4>
+                            <hr/>
+                            <p>Want to promote your business or provide sponsorship?</p>
+                            <i class="fas fa-envelope text-muted"></i><a href="mailto:promo@irievibes.be"> promo@irievibes.be</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-md-4 col-lg-3">
+                    <div class="text-box-2 rounded p-3">
+                        <div class="text-box rounded p-2">
+                            <h4>Tech</h4>
+                            <hr/>
+                            <p>For all your inquiries about our tech-magic.</p>
+                            <i class="fas fa-envelope text-muted"></i><a href="mailto:info@irievibes.be"> info@irievibes.be</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-md-4 col-lg-3">
+                    <div class="text-box-2 rounded p-3">
+                        <div class="text-box rounded p-2">
+                            <h4>General information</h4>
+                            <hr/>
+                            <p>For general questions that do not have a specific address</p>
+                            <i class="fas fa-envelope text-muted"></i><a href="mailto:info@irievibes.be"> info@irievibes.be</a>
+                        </div>
+                    </div>
+                </div>
             </div>
+            <p class="text-center">Found a bug in the website? Contact the webmaster: <a href="mailto:web@irievibes.be">web@irievibes.be</a>.</p> 
         </div>
-        <footer>   
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-3 text-center">
-                        <a href="/en/info/volunteers" class="btn btn-volunteer">Become Volunteer</a>
-                    </div>
-                    <div class="col-md-6 m-auto text-center d-none d-md-block"><p>Copyright &copy; - vzw GreenForward 2002-2019</p></div>
-                    <div class="col-md-3">
-                        <div id="btn-social" class="text-center">
-                            <a href="https://www.facebook.com/IrieVibesRootsFesitval"><i class="fab fa-facebook-square fa-3x social"></i></a>
-                            <a href="https://www.instagram.com/irievibesroots/"><i class="fab fa-instagram fa-3x social"></i></a>
-                            <a href="/src/static/snapchat/"><i class="fab fa-snapchat-square fa-3x social"></i></a>
-                        </div>
-                    </div>
-                    <!-- Redundant for Mobile Compatibility-->
-                    <div class="col-md-12 text-center d-block d-md-none"><p>Copyright &copy; - vzw GreenForward 2002-2019</p></div>
-                </div>
-            </div>
-        </footer>
+        <!-- Include Footer -->
+        <?php include($_SERVER['DOCUMENT_ROOT'].'/src/static/pages/footer_en.php');?>        
     </body>
     <!--Loading bootstrap and jQuery scripts at the end of the page to prevent slowdowns-->
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
-    <!-- Clickable Divs -->
-    <script>
-        $(".link").click(function() {
-            window.location = $(this).find("a").attr("href");
-            return false;
-        });
-    </script>
+    <?php include($_SERVER['DOCUMENT_ROOT'].'/src/static/pages/load_last.php');?>
 </html>
