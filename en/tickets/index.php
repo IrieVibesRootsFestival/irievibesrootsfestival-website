ed8dc91525d535e0e6498fb32cc6c461d92e2d78
Anna <djsndcx@gmail.com>
2019-11-13 19:53:10 +0100

    Update index.php

--- en/tickets/index.php
+++ en/tickets/index.php
@@ -17,7 +17,7 @@
                     <h3 class="display-7 text-center">Tickets</h3>
                     <p class="text-center text-irie"><b>July 16-17-18 2020</b></p>
                 </div>
-                <div class="rounded p-2 text-box-2"><div id="eventsquare-store" alt="Eventsquare Store"></div></div>
+                <div class="col-12 rounded p-2 text-box-2"><div id="eventsquare-store" alt="Eventsquare Store"></div></div>
                 <div id="tickets" class="col-12 text-center">
                     <h1>Presale Shops</h1>
                     <div class="row">
