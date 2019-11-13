42051bf3eccb2f66493511ddfd35c2c9ad5970aa
Anna <djsndcx@gmail.com>
2019-11-13 19:32:24 +0100

    new gif, ticets

--- src/js/eventsquare-embed.js
+++ src/js/eventsquare-embed.js
@@ -2,4 +2,4 @@
 s.callMethod.apply(s,arguments):s.queue.push(arguments)};s.queue=[];
 q=v.createElement('script');q.async=!0;q.src=t;r=v.getElementsByTagName(n)[0];
 r.parentNode.insertBefore(q,r)}(window,document,'script','https://s3-eu-west-1.amazonaws.com/eventsquare.plugins/embed/embed.min.js');
-esq('init','subdomain','embed','eventsquare-store');
+esq('init','irie-vibes','embed','eventsquare-store');
