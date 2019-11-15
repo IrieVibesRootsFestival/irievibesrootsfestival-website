7c7963b02a3e25ff9d8f8c69f0bc2ef2494c5ecf
Anna <djsndcx@gmail.com>
2019-11-15 10:19:46 +0100

    Navbar updates

--- src/static/pages/header_nl.php
+++ src/static/pages/header_nl.php
@@ -64,7 +64,7 @@
                         </ul>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link"href="#footer-anchor"><i class="fas fa-info-circle"></i><span class="d-lg-none"> Info</span></a>
+                        <a class="nav-link"href="#footer-anchor"><i class="fas fa-info-circle"></i> Info</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="#ShoppingCart"><i class="fas fa-shopping-cart"></i><span class="d-lg-none"> Merch Store</span></a>
