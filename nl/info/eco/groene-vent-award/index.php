a58fe37d785035f96ebf26938336861856ed5787
Anna <djsndcx@gmail.com>
2019-11-20 10:18:24 +0100

    Add GroenEvent Award page

--- nl/info/eco/groene-vent-award/index.php
+++ nl/info/eco/groene-vent-award/index.php
@@ -10,7 +10,27 @@
         <?php include($_SERVER['DOCUMENT_ROOT'].'/src/static/pages/header_nl.php');?>
         <div class="container content">         
             <h1 style="margin-bottom:5px" class="display-7 text-center">Groene Vent Award</h1>
+            <br/>
+            <div class="row">
+                <div class="col-12 col-md-9">
+                    <p>Wist je dat …</p>
+                    <ul>
+                        <li>we in 2017 behoorden tot de finalisten van het GroenEvent Award?</li>
+                        <li>onze ecologische initiatieven ervoor zorgen dat we in 2018 de GroenEvent Award mochten ontvangen binnen de categorie kleine evenementen?</li>
+                    </ul>
+                </div>
+                <div class="col-12 col-md-3 rounded">
+                    <div class="fb-video" data-href="https://www.facebook.com/facebook/videos/10153231379946729/" data-width="100%" data-show-text="false">
+                    </div>
+                </div>
             </div>
+            <div class="row">
+                <div class="col-12 col-md-3"><img class="img-fluid rounded" src="/src/img/eco/ge-0.webp"/></div>
+                <div class="col-12 col-md-3"><img class="img-fluid rounded" src="/src/img/eco/ge-1.webp"/></div>
+                <div class="col-12 col-md-3"><img class="img-fluid rounded" src="/src/img/eco/ge-2.webp"/></div>
+                <div class="col-12 col-md-3"><img class="img-fluid rounded" src="/src/img/eco/ge-3.webp"/></div>
+            </div>
+        </div>
         <!-- Include Footer -->
         <?php include($_SERVER['DOCUMENT_ROOT'].'/src/static/pages/footer_nl.php');?>        
     </body>
