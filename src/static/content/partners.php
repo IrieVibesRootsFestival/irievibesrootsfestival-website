20b70c60edf52dec9ce8568bb40a6676d7c70519
Anna <djsndcx@gmail.com>
2019-11-23 13:02:00 +0100

    Update partners, change spacing

--- src/static/content/partners.php
+++ src/static/content/partners.php
@@ -1,298 +1,298 @@
-<div class="col-6 col-sm-4 col-md-3 my-2">
+<div class="col-6 col-sm-3 col-md-2 my-2">
     <div class="thumbnail">
         <a href="http://www.alkover.be">
             <img class="img-fluid rounded" src="/src/img/partners/alkover.jpg" alt="Alkover">
         </a>
     </div>
 </div>
-<div class="col-6 col-sm-4 col-md-3 my-2">
+<div class="col-6 col-sm-3 col-md-2 my-2">
     <div class="thumbnail">
         <a href="http://www.hoeves.be/ijzerweelde/ijzerweelde">
             <img class="img-fluid rounded" src="/src/img/partners/ijzerweelde.jpg" alt="Vakantiehoeves Ijzerweelde">
         </a>
     </div>
 </div>
-<div class="col-6 col-sm-4 col-md-3 my-2">
+<div class="col-6 col-sm-3 col-md-2 my-2">
     <div class="thumbnail">
         <a href="http://www.terranis.be/">
             <img class="img-fluid rounded" src="/src/img/partners/terranis.jpg" alt="Terranis">
         </a>
     </div>
 </div>
-<div class="col-6 col-sm-4 col-md-3 my-2">
+<div class="col-6 col-sm-3 col-md-2 my-2">
     <div class="thumbnail">
         <a href="http://www.cm.be/">
             <img class="img-fluid rounded" src="/src/img/partners/cm.jpg" alt="Christelijke Mutualiteit">
         </a>
     </div>
 </div>
-<div class="col-6 col-sm-4 col-md-3 my-2">
+<div class="col-6 col-sm-3 col-md-2 my-2">
     <div class="thumbnail">
         <a href="http://www.dewofire.be/">
             <img class="img-fluid rounded" src="/src/img/partners/dewofire.jpg" alt="Dewofire">
         </a>
     </div>
 </div>
-<div class="col-6 col-sm-4 col-md-3 my-2">
+<div class="col-6 col-sm-3 col-md-2 my-2">
     <div class="thumbnail">
         <a href="https://www.tenboogaerde.be/">
             <img class="img-fluid rounded" src="/src/img/partners/tenboogaerde.jpg" alt="Ten Boogaerde">
         </a>
     </div>
 </div>
-<div class="col-6 col-sm-4 col-md-3 my-2">
+<div class="col-6 col-sm-3 col-md-2 my-2">
     <div class="thumbnail">
         <a href="https://www.vdmachinery.be">
             <img class="img-fluid rounded" src="/src/img/partners/vdmachinery.jpg" alt="VD Machinery">
         </a>
     </div>
 </div>
-<div class="col-6 col-sm-4 col-md-3 my-2">
+<div class="col-6 col-sm-3 col-md-2 my-2">
     <div class="thumbnail">
         <a href="http://www.bloom-diksmuide.be/">
             <img class="img-fluid rounded" src="/src/img/partners/bloom.jpg" alt="Bloom">
         </a>
     </div>
 </div>
-<div class="col-6 col-sm-4 col-md-3 my-2">
+<div class="col-6 col-sm-3 col-md-2 my-2">
     <div class="thumbnail">
         <a href="https://www.renewi.be">
             <img class="img-fluid rounded" src="/src/img/partners/renewi.jpg" alt="Renewi">
         </a>
     </div>
 </div>
-<div class="col-6 col-sm-4 col-md-3 my-2">
+<div class="col-6 col-sm-3 col-md-2 my-2">
     <div class="thumbnail">
         <a href="https://kantoorvolcke.be/">
             <img class="img-fluid rounded" src="/src/img/partners/crelan.jpg" alt="Crelan Kortemark">
         </a>
     </div>
 </div>
-<div class="col-6 col-sm-4 col-md-3 my-2">
+<div class="col-6 col-sm-3 col-md-2 my-2">
     <div class="thumbnail">
         <a href="http://in-print.be/">
             <img class="img-fluid rounded" src="/src/img/partners/inprint.jpg" alt="In-Print">
         </a>
     </div>
 </div>
-<div class="col-6 col-sm-4 col-md-3 my-2">
+<div class="col-6 col-sm-3 col-md-2 my-2">
     <div class="thumbnail">
         <a href="https://www.vks-koeling.be/">
             <img class="img-fluid rounded" src="/src/img/partners/vks.jpg" alt="VKS Koeling">
         </a>
     </div>
 </div>
-<div class="col-6 col-sm-4 col-md-3 my-2">
+<div class="col-6 col-sm-3 col-md-2 my-2">
     <div class="thumbnail">
         <a href="https://www.shellys.be/">
             <img class="img-fluid rounded" src="/src/img/partners/shellys.jpg" alt="Frituur Shelly's">
         </a>
     </div>
 </div>
-<div class="col-6 col-sm-4 col-md-3 my-2">
+<div class="col-6 col-sm-3 col-md-2 my-2">
     <div class="thumbnail">
         <a href="https://www.vanhamtenten.com">
             <img class="img-fluid rounded" src="/src/img/partners/vanham.jpg" alt="Tenten Vanham">
         </a>
     </div>
 </div>
-<div class="col-6 col-sm-4 col-md-3 my-2">
+<div class="col-6 col-sm-3 col-md-2 my-2">
     <div class="thumbnail">
         <a href="https://www.feestmetmij.be">
             <img class="img-fluid rounded" src="/src/img/partners/feestmetmij.jpg" alt="Feest Met Mij">
         </a>
     </div>
 </div>
-<div class="col-6 col-sm-4 col-md-3 my-2">
+<div class="col-6 col-sm-3 col-md-2 my-2">
     <div class="thumbnail">
         <a href="https://www.worldwineweb.be">
             <img class="img-fluid rounded" src="/src/img/partners/wwinew.jpg" alt="World Wine Web">
         </a>
     </div>
 </div>
-<div class="col-6 col-sm-4 col-md-3 my-2">
+<div class="col-6 col-sm-3 col-md-2 my-2">
     <div class="thumbnail">
         <a href="http://www.ondernemers-en-shoppingnetwerk.be/Kortemark-8610/Proxy-Delhaize-Zarren/6590/overons/418/gemeentee">
             <img class="img-fluid rounded" src="/src/img/partners/delhaizezarren.jpg" alt="Delhaize Zarren">
         </a>
     </div>
 </div>
-<div class="col-6 col-sm-4 col-md-3 my-2">
+<div class="col-6 col-sm-3 col-md-2 my-2">
     <div class="thumbnail">
         <a href="http://www.realtimesystems.be/">
             <img class="img-fluid rounded" src="/src/img/partners/realtimesystems.jpg" alt="Realtime Systems">
         </a>
     </div>
 </div>
-<div class="col-6 col-sm-4 col-md-3 my-2">
+<div class="col-6 col-sm-3 col-md-2 my-2">
     <div class="thumbnail">
         <a href="http://www.toekoertoilets.be/">
             <img class="img-fluid rounded" src="/src/img/partners/toekoer.jpg" alt="Toekoer Toiletten">
         </a>
     </div>
 </div>
-<div class="col-6 col-sm-4 col-md-3 my-2">
+<div class="col-6 col-sm-3 col-md-2 my-2">
     <div class="thumbnail">
         <a href="#">
             <img class="img-fluid rounded" src="/src/img/partners/bddmechanic.jpg" alt="BDD Mechanic Solutions">
         </a>
     </div>
 </div>
-<div class="col-6 col-sm-4 col-md-3 my-2">
+<div class="col-6 col-sm-3 col-md-2 my-2">
     <div class="thumbnail">
         <a href="http://www.restaurant-hapkin.be/">
             <img class="img-fluid rounded" src="/src/img/partners/hapkin.jpg" alt="Restaurant Hapkin">
         </a>
     </div>
 </div>
-<div class="col-6 col-sm-4 col-md-3 my-2">
+<div class="col-6 col-sm-3 col-md-2 my-2">
     <div class="thumbnail">
         <a href="www.rojo.be/">
             <img class="img-fluid rounded" src="/src/img/partners/rojo.jpg" alt="Rojo">
         </a>
     </div>
 </div>
-<div class="col-6 col-sm-4 col-md-3 my-2">
+<div class="col-6 col-sm-3 col-md-2 my-2">
     <div class="thumbnail">
         <a href="https://www.kbc.be/retail/en/branch/8217-KORTEMARK-MARKT-22.html">
             <img class="img-fluid rounded" src="/src/img/partners/kbc.jpg" alt="KBC">
         </a>
     </div>
 </div>
-<div class="col-6 col-sm-4 col-md-3 my-2">
+<div class="col-6 col-sm-3 col-md-2 my-2">
     <div class="thumbnail">
         <a href="https://www.goldenpages.be/company/Kortemark/L2744606/Elektro+Maarten+Gotelaere/">
             <img class="img-fluid rounded" src="/src/img/partners/maartengotelaere.jpg" alt="Maarten Gotelaere">
         </a>
     </div>
 </div>
-<div class="col-6 col-sm-4 col-md-3 my-2">
+<div class="col-6 col-sm-3 col-md-2 my-2">
     <div class="thumbnail">
         <a href="http://www.mokup.be">
             <img class="img-fluid rounded" src="/src/img/partners/mokup.jpg" alt="Mokup">
         </a>
     </div>
 </div>
-<div class="col-6 col-sm-4 col-md-3 my-2">
+<div class="col-6 col-sm-3 col-md-2 my-2">
     <div class="thumbnail">
         <a href="http://b4plastics.com">
             <img class="img-fluid rounded" src="/src/img/partners/b4plastics.jpg" alt="B4 Plastics">
         </a>
     </div>
 </div>
-<div class="col-6 col-sm-4 col-md-3 my-2">
+<div class="col-6 col-sm-3 col-md-2 my-2">
     <div class="thumbnail">
         <a href="https://mooimakers.be/">
             <img class="img-fluid rounded" src="/src/img/partners/mooimakers.jpg" alt="Mooimakers">
         </a>
     </div>
 </div>
-<div class="col-6 col-sm-4 col-md-3 my-2">
+<div class="col-6 col-sm-3 col-md-2 my-2">
     <div class="thumbnail">
         <a href="http://verspuntkortemark.be/">
             <img class="img-fluid rounded" src="/src/img/partners/verspuntkortemark.jpg" alt="Verspunt Kortemark">
         </a>
     </div>
 </div>
-<div class="col-6 col-sm-4 col-md-3 my-2">
+<div class="col-6 col-sm-3 col-md-2 my-2">
     <div class="thumbnail">
         <a href="http://dedollebrouwers.be">
             <img class="img-fluid rounded" src="/src/img/partners/dedollebrouwers.jpg" alt="De Dolle Brouwers">
         </a>
     </div>
 </div>
-<div class="col-6 col-sm-4 col-md-3 my-2">
+<div class="col-6 col-sm-3 col-md-2 my-2">
     <div class="thumbnail">
         <a href="http://www.gsnel.eu/en/">
             <img class="img-fluid rounded" src="/src/img/partners/snel.jpg" alt="SNEL Logistics">
         </a>
     </div>
 </div>
-<div class="col-6 col-sm-4 col-md-3 my-2">
+<div class="col-6 col-sm-3 col-md-2 my-2">
     <div class="thumbnail">
         <a href="http://www.aes-services.be/">
             <img class="img-fluid rounded" src="/src/img/partners/aes.jpg" alt="AES Podia">
         </a>
     </div>
 </div>
-<div class="col-6 col-sm-4 col-md-3 my-2">
+<div class="col-6 col-sm-3 col-md-2 my-2">
     <div class="thumbnail">
         <a href="http://www.ebs-sound-light.be/nl/">
             <img class="img-fluid rounded" src="/src/img/partners/ebssoundlight.jpg" alt="EBS Sound & Light">
         </a>
     </div>
 </div>
-<div class="col-6 col-sm-4 col-md-3 my-2">
+<div class="col-6 col-sm-3 col-md-2 my-2">
     <div class="thumbnail">
         <a href="http://www.slagerij-jan.be/">
             <img class="img-fluid rounded" src="/src/img/partners/slagjan.jpg" alt="SLagerij Jan">
         </a>
     </div>
 </div>
-<div class="col-6 col-sm-4 col-md-3 my-2">
+<div class="col-6 col-sm-3 col-md-2 my-2">
     <div class="thumbnail">
         <a href="https://winkels.carrefour.eu/nl/s/carrefour/carrefour-market-kortemark/358">
             <img class="img-fluid rounded" src="/src/img/partners/carrefourkortemark.jpg" alt="Carrefour Kortemark">
         </a>
     </div>
 </div>
-<div class="col-6 col-sm-4 col-md-3 my-2">
+<div class="col-6 col-sm-3 col-md-2 my-2">
     <div class="thumbnail">
         <a href="https://www.defarmerie.be/">
             <img class="img-fluid rounded" src="/src/img/partners/defarmerie.jpg" alt="Carrefour Kortemark">
         </a>
     </div>
 </div>
-<div class="col-6 col-sm-4 col-md-3 my-2">
+<div class="col-6 col-sm-3 col-md-2 my-2">
     <div class="thumbnail">
         <a href="https://www.desotec.com/en">
             <img class="img-fluid rounded" src="/src/img/partners/desotec.jpg" alt="Desotec">
         </a>
     </div>
 </div>
-<div class="col-6 col-sm-4 col-md-3 my-2">
+<div class="col-6 col-sm-3 col-md-2 my-2">
     <div class="thumbnail">
         <a href="https://www.desimpelenergygroup.be/tankstations/delishop_kortemark_21/">
             <img class="img-fluid rounded" src="/src/img/partners/desimpel.jpg" alt="Desimpel Kortemark">
         </a>
     </div>
 </div>
-<div class="col-6 col-sm-4 col-md-3 my-2">
+<div class="col-6 col-sm-3 col-md-2 my-2">
     <div class="thumbnail">
         <a href="https://avevewinkels.be/Winkels/verbraeken-davy-bvba/060341">
             <img class="img-fluid rounded" src="/src/img/partners/aveveroeselare.jpg" alt="Aveve">
         </a>
     </div>
 </div>
-<div class="col-6 col-sm-4 col-md-3 my-2">
+<div class="col-6 col-sm-3 col-md-2 my-2">
     <div class="thumbnail">
         <a href="http://www.stones-and-tiles.be/">
             <img class="img-fluid rounded" src="/src/img/partners/stonesandtiles.jpg" alt="Stones and Tiles">
         </a>
     </div>
 </div>
-<div class="col-6 col-sm-4 col-md-3 my-2">
+<div class="col-6 col-sm-3 col-md-2 my-2">
     <div class="thumbnail">
         <a href="https://dewatergroep.be/">
             <img class="img-fluid rounded" src="/src/img/partners/dewatergroep.jpg" alt="De Watergroep">
         </a>
     </div>
 </div>
-<div class="col-6 col-sm-4 col-md-3 my-2">
+<div class="col-6 col-sm-3 col-md-2 my-2">
     <div class="thumbnail">
         <a href="https://www.facebook.com/frituuredewalle/">
             <img class="img-fluid rounded" src="/src/img/partners/frituuredewalle.jpg" alt="Frituur Edewalle">
         </a>
     </div>
 </div>
-<div class="col-6 col-sm-4 col-md-3 my-2">
+<div class="col-6 col-sm-3 col-md-2 my-2">
     <div class="thumbnail">
         <a href="https://www.facebook.com/aardbeiendavidenangie">
             <img class="img-fluid rounded" src="/src/img/partners/davidenangie.jpg" alt="Fruit & Groenten David & Angie">
         </a>
     </div>
 </div>
-<div class="col-6 col-sm-4 col-md-3 my-2">
+<div class="col-6 col-sm-3 col-md-2 my-2">
     <div class="thumbnail">
         <a href="https://www.interregeurope.eu/">
             <img class="img-fluid rounded" src="/src/img/partners/interreg.jpg" alt="Interreg - France Flanders Wallonie">
