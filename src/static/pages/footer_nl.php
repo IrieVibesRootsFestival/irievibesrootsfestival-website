44175188b1c8c8e1ba51ae4977f992d1bc01eeda
Anna <djsndcx@gmail.com>
2019-12-19 11:09:38 +0100

    Correcting html cleanliness

--- src/static/pages/footer_nl.php
+++ src/static/pages/footer_nl.php
@@ -3,7 +3,7 @@
     <div class="container pt-2">
         <div class="row">
             <div class="col-12 col-md-3 footer-brand animated fadeInLeft text-center">
-                <img alt="" src="/src/img/greenforward-large.webp" class="img-fluid p-3 rounded-circle" alt="Greenforward Logo">
+                <img src="/src/img/greenforward-large.webp" class="img-fluid p-3 rounded-circle" alt="Greenforward Logo">
             </div>
             <div class="col-12 col-md-6 footer-nav animated fadeInUp">
                 <h4>— Menu —</h4>
