86ea658187130ff42961b5e02e15f07c44a6d329
Anna <djsndcx@gmail.com>
2019-12-26 09:38:35 +0100

    Adding Spellbreakers Facebook Video

--- en/lineup/saturday.php
+++ en/lineup/saturday.php
@@ -1,56 +1,28 @@
-<div id="lineup" class="row text-center d-none">
+<div id="lineup" class="row text-center">
     <!-- Lion's Den -->
     <div id="stage_tent" class="col-md-6">
         <div class="row py-2 mb-3 border border-lg border-success rounded bg-img">
             <div class="col-12"><p class="text-center bg-success rounded text-white">Lion's Den</p></div>
             <div class="col-6">
-                <div class="text-white bg-dark px-1 rounded h4">14:00</div>
-                <div class="img-fluid">    
-                    <img alt="" class="img-fluid rounded" src="/src/img/artists/sunny-after-moon.jpg"> 
+                <div class="text-white bg-dark px-1 rounded h4">TEASER</div>
+                <div class="facebook-responsive">
+                    <iframe src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fwatch%2F%3Fv%3D1062072777464176&width=500&show_text=false&height=280&appId" width="500" height="280" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media" allowFullScreen="true"></iframe>
                 </div>
-                <a class="d-block text-center pt-2" href="https://www.facebook.com/sunnyaftermoon/">Sunny After Moon</a>
+                <a class="d-block text-center pt-2" href="https://www.facebook.com/Spellbreakersband">Spellbreakers</a>
             </div>
-            <div class="col-6">
-                <div class="text-white bg-dark px-1 rounded h4">16:15</div>
-                <div class="img-fluid img-container">
-                    <img alt="" class="img-fluid rounded" src="/src/img/artists/donovan-kingjay.jpg"> 
-                </div>
-                <a class="d-block text-center pt-2" href="http://donovankingjay.com/">Donovan Kingjay</a>
+
+        </div>
+        <!-- End singer & band block-->
+        <div class="col-6 d-none">
+            <div class="text-white bg-dark px-1 rounded h4">23:30</div>
+            <div class="img-fluid img-container">
+                <img alt="" class="img-fluid rounded" src="/src/img/artists/jahmbassador.jpg"> 
             </div>
-            <div class="col-6">
-                <div class="text-white bg-dark px-1 rounded h4">19:00</div>
-                <div class="img-fluid img-container">
-                    <img alt="" class="img-fluid rounded" src="/src/img/artists/new-kingston.jpg"> 
-                </div>
-                <a class="d-block text-center pt-2" href="http://newkingstonmusic.com/">New Kingston</a>
-            </div>
-            <!-- Singer + Band block-->
-            <div class="row border border-lg border-dark rounded m-2">
-                <div class="col-12 px-0"><div class="text-white bg-dark px-1 h4">22:00</div></div>
-                <div class="col-6">
-                    <div class="img-fluid img-container">
-                        <img alt="" class="img-fluid rounded" src="/src/img/artists/uwe-banton.jpg"> 
-                    </div>
-                    <a class="d-block text-center pt-2" href="http://www.uwebanton.com/">Uwe Banton <small class="text-danger"></small></a>
-                </div>
-                <div class="col-6">
-                    <div class="img-fluid img-container">
-                        <img alt="" class="img-fluid rounded" src="/src/img/artists/toughcutt-band.jpg"> 
-                    </div>
-                    <a class="d-block text-center pt-2" href="https://www.facebook.com/ToughCutt-Band-520781848077575/">Toughcutt Band<small class="text-danger"></small></a>
-                </div>
-            </div>
-            <!-- End singer & band block-->
-            <div class="col-6">
-                <div class="text-white bg-dark px-1 rounded h4">23:30</div>
-                <div class="img-fluid img-container">
-                    <img alt="" class="img-fluid rounded" src="/src/img/artists/jahmbassador.jpg"> 
-                </div>
-                <a class="d-block text-center pt-2" href="https://www.jahmbassador.be/events/detail/irie-vibes">Jahmbassador</a>
-            </div>
+            <a class="d-block text-center pt-2" href="https://www.jahmbassador.be/events/detail/irie-vibes">Jahmbassador</a>
         </div>
     </div>
-    <!-- Open Air
+</div>
+<!-- Open Air
 <div div="stage_air" class="col-md-6">
 <div class="row py-2 mb-3 border border-lg border-danger rounded bg-img">
 <div class="col-12"><p class="text-center bg-danger rounded text-white">Open Air</p></div>
@@ -58,59 +30,59 @@
 </div> 
 -->
 
-    <!-- Little Lion-->
-    <div id="wrapper" class="col-md-6 px-0">
-        <!-- Inner Circle -->
-        <div id="stage_tent_outside" class="col-12">
-            <div class="row py-2 mb-3 border border-lg border-sand rounded bg-img">
-                <div class="col-12"><p class="bg-sand text-center rounded text-white">Inner Circle</p></div>
-                <div class="col-6">
-                    <div class="text-white bg-dark px-1 rounded h4">12:00</div>
-                    <div class="img-fluid img-container">
-                        <img alt="" class="img-fluid rounded" src="/src/img/artists/sattasound-babbajah.jpg"></div>
-                    <a class="d-block text-center pt-2" href="https://www.facebook.com/sattasoundsystem/">Satta Sound ft. Babbajah</a>
-                </div>
-                <div class="col-6">
-                    <div class="text-white bg-dark px-1 rounded h4">15:00</div>
-                    <div class="img-fluid img-container">
-                        <img alt="" class="img-fluid rounded" src="/src/img/artists/fishermen-sound.jpg">
-                    </div>
-                    <a class="d-block text-center pt-2" href="https://www.facebook.com/FisherMenDub/">Fishermen Sound</a>
-                </div>
-                <div class="col-6">
-                    <div class="text-white bg-dark px-1 rounded h4">17:15</div>
-                    <div class="img-fluid img-container">
-                        <img alt="" class="img-fluid rounded" src="/src/img/artists/forward-fever.jpg">
-                    </div>
-                    <a class="d-block text-center pt-2" href="https://www.facebook.com/forward.fever/">Forward Fever</a>
-                </div>
-                <div class="col-6">
-                    <div class="text-white bg-dark px-1 rounded h4">20:00</div>
-                    <div class="img-fluid img-container">
-                        <img alt="" class="img-fluid rounded" src="/src/img/artists/earthworks.jpg">
-                    </div>
-                    <a class="d-block text-center pt-2" href="https://www.earthworksamsterdam.com/">Earthworks</a>
-                </div>
+<!-- Little Lion-->
+<div id="wrapper" class="col-md-6 px-0">
+    <!-- Inner Circle -->
+    <div id="stage_tent_outside" class="col-12">
+        <div class="row py-2 mb-3 border border-lg border-sand rounded bg-img">
+            <div class="col-12"><p class="bg-sand text-center rounded text-white">Inner Circle</p></div>
+            <div class="col-6">
+                <div class="text-white bg-dark px-1 rounded h4">12:00</div>
+                <div class="img-fluid img-container">
+                    <img alt="" class="img-fluid rounded" src="/src/img/artists/sattasound-babbajah.jpg"></div>
+                <a class="d-block text-center pt-2" href="https://www.facebook.com/sattasoundsystem/">Satta Sound ft. Babbajah</a>
             </div>
-        </div>
-        <div id="stage_kids" class="col-12">
-            <!-- Kinderdorp -->
-            <div class="row py-2 mb-3 border border-lg border-warning rounded bg-img">
-                <div class="col-12"><p class="bg-warning rounded text-center text-white">Little Lion</p></div>
-                <div class="col-12"><div class="text-white bg-dark p-1 rounded h4">Continuous</div></div>
-                <div class="col-6">
-                    <div class="img-fluid img-container">
-                        <img alt="" class="img-fluid rounded" src="/src/img/artists/funny_haircuts.jpg">
-                    </div>
-                    <a class="d-block pt-2" href="">Crazy Hairstyles</a>
+            <div class="col-6">
+                <div class="text-white bg-dark px-1 rounded h4">15:00</div>
+                <div class="img-fluid img-container">
+                    <img alt="" class="img-fluid rounded" src="/src/img/artists/fishermen-sound.jpg">
                 </div>
-                <div class="col-6">
-                    <div class="img-fluid img-container">
-                        <img alt="" class="img-fluid rounded" src="/src/img/artists/making_jewelry.jpg"> 
-                    </div>
-                    <a class="d-block pt-2" href="">Making Jewelry</a>                
+                <a class="d-block text-center pt-2" href="https://www.facebook.com/FisherMenDub/">Fishermen Sound</a>
+            </div>
+            <div class="col-6">
+                <div class="text-white bg-dark px-1 rounded h4">17:15</div>
+                <div class="img-fluid img-container">
+                    <img alt="" class="img-fluid rounded" src="/src/img/artists/forward-fever.jpg">
                 </div>
+                <a class="d-block text-center pt-2" href="https://www.facebook.com/forward.fever/">Forward Fever</a>
+            </div>
+            <div class="col-6">
+                <div class="text-white bg-dark px-1 rounded h4">20:00</div>
+                <div class="img-fluid img-container">
+                    <img alt="" class="img-fluid rounded" src="/src/img/artists/earthworks.jpg">
+                </div>
+                <a class="d-block text-center pt-2" href="https://www.earthworksamsterdam.com/">Earthworks</a>
             </div>
         </div>
     </div>
+    <div id="stage_kids" class="col-12">
+        <!-- Kinderdorp -->
+        <div class="row py-2 mb-3 border border-lg border-warning rounded bg-img">
+            <div class="col-12"><p class="bg-warning rounded text-center text-white">Little Lion</p></div>
+            <div class="col-12"><div class="text-white bg-dark p-1 rounded h4">Continuous</div></div>
+            <div class="col-6">
+                <div class="img-fluid img-container">
+                    <img alt="" class="img-fluid rounded" src="/src/img/artists/funny_haircuts.jpg">
+                </div>
+                <a class="d-block pt-2" href="">Crazy Hairstyles</a>
+            </div>
+            <div class="col-6">
+                <div class="img-fluid img-container">
+                    <img alt="" class="img-fluid rounded" src="/src/img/artists/making_jewelry.jpg"> 
+                </div>
+                <a class="d-block pt-2" href="">Making Jewelry</a>                
+            </div>
+        </div>
+    </div>
+</div>
 </div>
