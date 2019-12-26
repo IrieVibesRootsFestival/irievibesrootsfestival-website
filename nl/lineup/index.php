6d98cc0fb2ea7c3ff5dcfe6ff380f4fc2d968531
Anna <djsndcx@gmail.com>
2019-12-26 11:41:53 +0100

    Renewing lineup

---
+++ nl/lineup/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="nl">
+    <head>
+        <!-- Import meta tags, OpenGraphs, stylesheets and common includes -->
+        <?php include($_SERVER['DOCUMENT_ROOT'].'/src/static/pages/head_common.php');?>
+        <title>Line-Up - Irie Vibes</title>
+        <meta property="og:title" content="Line-Up"/>
+    </head>
+    <body>
+        <?php include($_SERVER['DOCUMENT_ROOT'].'/src/static/pages/header_nl.php');?>
+        <!-- This is the main container that encapsulates everything content related. -->
+        <div id="lineup-home" class="container">
+            <div class="row">
+                <div class="col-12 text-center"><span class="display-7">Line-up Index</span></div>
+                <div class="col-12">
+                    <div class="text-center">
+                        <div class="btn-group mb-1" role="group" aria-label="day-buttons">
+                            <a href="#lineup-thursday"><button class="btn btn-irie"><p>Donderdag</p></button></a>
+                            <a href="#lineup-friday"><button class="btn btn-irie"><p>Vrijdag/p></button></a>
+                            <a href="#lineup-saturday"><button class="btn btn-irie"><p>Zaterdag</p></button></a>
+                        </div>
+                    </div>
+                    <a id="lineup-thursday"/>
+                    <h3 class="text-irie"><a href="/nl/lineup/kortemark-conge/"><b><i class="fas fa-link fa-xs" style="font-size: 0.6rem;"></i>Donderdag (Kortemark Congé)</b></a></h3>
+                    <?php include($_SERVER['DOCUMENT_ROOT'].'/src/static/lineup/kortemark-conge.php');?>
+                    <a id="lineup-friday"/>
+                    <h3 class="text-irie"><a href="/nl/lineup/friday/"><b><i class="fas fa-link fa-xs" style="font-size: 0.6rem;"></i>Vrijdag</b></a></h3>
+                    <?php include($_SERVER['DOCUMENT_ROOT'].'/src/static/lineup/friday.php');?>
+                    <a id="lineup-saturday"/>
+                    <h3 class="text-irie"><a href="/nl/lineup/saturday/"><b><i class="fas fa-link fa-xs" style="font-size: 0.6rem;"></i>Zaterdag</b></a></h3>
+                    <?php include($_SERVER['DOCUMENT_ROOT'].'/src/static/lineup/saturday.php');?>
+                </div>
+            </div>
+        </div>
+        <!-- Include Footer -->
+        <?php include($_SERVER['DOCUMENT_ROOT'].'/src/static/pages/footer_en.php');?>        
+    </body>
+    <!--Loading bootstrap and jQuery scripts at the end of the page to prevent slowdowns-->
+    <?php include($_SERVER['DOCUMENT_ROOT'].'/src/static/pages/load_last.php');?>
+</html>
---
+++ src/lineup/saturday.php
@@ -0,0 +1,87 @@
+<div id="lineup" class="row text-center">
+    <!-- Lion's Den -->
+    <div id="stage_tent" class="col-md-6">
+        <div class="row py-2 mb-3 border border-lg border-success rounded bg-img">
+            <div class="col-12"><p class="text-center bg-success rounded text-white">Lion's Den</p></div>
+            <div class="col-12">
+                <div class="text-white bg-dark px-1 rounded h4">TEASER</div>
+                <div class="facebook-responsive">
+                    <iframe src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fwatch%2F%3Fv%3D1062072777464176&width=500&show_text=false&height=280&appId" width="" height="" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media" allowFullScreen="true"></iframe>
+                </div>
+                <a class="d-block text-center pt-2" href="https://www.facebook.com/Spellbreakersband">Spellbreakers</a>
+            </div>
+
+        </div>
+        <!-- End singer & band block-->
+        <div class="col-6 d-none">
+            <div class="text-white bg-dark px-1 rounded h4">23:30</div>
+            <div class="img-fluid img-container">
+                <img alt="" class="img-fluid rounded" src="/src/img/artists/jahmbassador.jpg"> 
+            </div>
+            <a class="d-block text-center pt-2" href="https://www.jahmbassador.be/events/detail/irie-vibes">Jahmbassador</a>
+        </div>
+    </div>
+</div>
+<!-- Open Air
+<div div="stage_air" class="col-md-6">
+<div class="row py-2 mb-3 border border-lg border-danger rounded bg-img">
+<div class="col-12"><p class="text-center bg-danger rounded text-white">Open Air</p></div>
+</div>
+</div> 
+-->
+
+<!-- Little Lion-->
+<div id="wrapper" class="col-md-6 px-0 d-none">
+    <!-- Inner Circle -->
+    <div id="stage_tent_outside" class="col-12">
+        <div class="row py-2 mb-3 border border-lg border-sand rounded bg-img">
+            <div class="col-12"><p class="bg-sand text-center rounded text-white">Inner Circle</p></div>
+            <div class="col-6">
+                <div class="text-white bg-dark px-1 rounded h4">12:00</div>
+                <div class="img-fluid img-container">
+                    <img alt="" class="img-fluid rounded" src="/src/img/artists/sattasound-babbajah.jpg"></div>
+                <a class="d-block text-center pt-2" href="https://www.facebook.com/sattasoundsystem/">Satta Sound ft. Babbajah</a>
+            </div>
+            <div class="col-6">
+                <div class="text-white bg-dark px-1 rounded h4">15:00</div>
+                <div class="img-fluid img-container">
+                    <img alt="" class="img-fluid rounded" src="/src/img/artists/fishermen-sound.jpg">
+                </div>
+                <a class="d-block text-center pt-2" href="https://www.facebook.com/FisherMenDub/">Fishermen Sound</a>
+            </div>
+            <div class="col-6">
+                <div class="text-white bg-dark px-1 rounded h4">17:15</div>
+                <div class="img-fluid img-container">
+                    <img alt="" class="img-fluid rounded" src="/src/img/artists/forward-fever.jpg">
+                </div>
+                <a class="d-block text-center pt-2" href="https://www.facebook.com/forward.fever/">Forward Fever</a>
+            </div>
+            <div class="col-6">
+                <div class="text-white bg-dark px-1 rounded h4">20:00</div>
+                <div class="img-fluid img-container">
+                    <img alt="" class="img-fluid rounded" src="/src/img/artists/earthworks.jpg">
+                </div>
+                <a class="d-block text-center pt-2" href="https://www.earthworksamsterdam.com/">Earthworks</a>
+            </div>
+        </div>
+    </div>
+    <div id="stage_kids" class="col-12">
+        <!-- Kinderdorp -->
+        <div class="row py-2 mb-3 border border-lg border-warning rounded bg-img">
+            <div class="col-12"><p class="bg-warning rounded text-center text-white">Little Lion</p></div>
+            <div class="col-12"><div class="text-white bg-dark p-1 rounded h4">Continuous</div></div>
+            <div class="col-6">
+                <div class="img-fluid img-container">
+                    <img alt="" class="img-fluid rounded" src="/src/img/artists/funny_haircuts.jpg">
+                </div>
+                <a class="d-block pt-2" href="">Crazy Hairstyles</a>
+            </div>
+            <div class="col-6">
+                <div class="img-fluid img-container">
+                    <img alt="" class="img-fluid rounded" src="/src/img/artists/making_jewelry.jpg"> 
+                </div>
+                <a class="d-block pt-2" href="">Making Jewelry</a>                
+            </div>
+        </div>
+    </div>
+</div>
