254ed9bac831fa49390f15d5bfdb1cbe750f02bd
Anna <djsndcx@gmail.com>
2019-12-26 18:56:32 +0100

    fix buttons and nav

--- nl/lineup/index.php
+++ nl/lineup/index.php
@@ -14,41 +14,41 @@
             <div class="row">
                 <div class="col-12 col-lg-3 order-lg-2">
                     <div class="text-box-2 rounded p-2 sticky-top mt-1">
-                        <div class="button-group-vertical d-flex">
-                            <button class="btn btn-irie"><a href="#lineup-thursday"><p>Donderdag</p></a></button>
-                            <button class="btn btn-irie"><a href="#lineup-friday"><p>Vrijdag</p></a></button>
-                            <button class="btn btn-irie"><a href="#lineup-saturday"><p>Zaterdag</p></a></button>
+                        <div class="btn-group-vertical d-flex text-box">
+                            <a class="btn" href="#lineup-thursday"><p>Donderdag</p></a>
+                            <button class="btn"><a href="#lineup-friday"><p>Vrijdag</p></a></button>
+                            <button class="btn"><a href="#lineup-saturday"><p>Zaterdag</p></a></button>
                         </div>
                     </div>
-                    <div class="col-12 col-lg-9">
-                        <a id="lineup-thursday"></a>
-                        <h3 class="text-irie text-uppercase">
-                            <b>Donderdag (Kortemark Congé) 
-                                <a href="/nl/lineup/kortemark-conge/">
-                                    <i class="fas fa-link fa-xs" style="font-size: 0.6rem;"></i>
-                                </a>
-                            </b>
-                        </h3>
-                        <?php include($_SERVER['DOCUMENT_ROOT'].'/src/static/lineup/kortemark-conge.php');?>
-                        <a id="lineup-friday"></a>
-                        <h3 class="text-irie text-uppercase">
-                            <b>Vrijdag 
-                                <a href="/nl/lineup/friday/">
-                                    <i class="fas fa-link fa-xs" style="font-size: 0.6rem;"></i>
-                                </a>
-                            </b>
-                        </h3>
-                        <?php include($_SERVER['DOCUMENT_ROOT'].'/src/static/lineup/friday.php');?>
-                        <a id="lineup-saturday"></a>
-                        <h3 class="text-irie text-uppercase">
-                            <b>Zaterdag 
-                                <a href="/nl/lineup/saturday/">
-                                    <i class="fas fa-link fa-xs" style="font-size: 0.6rem;"></i>
-                                </a>
-                            </b>
-                        </h3>
-                        <?php include($_SERVER['DOCUMENT_ROOT'].'/src/static/lineup/saturday.php');?>
-                    </div>
+                </div>
+                <div class="col-12 col-lg-9">
+                    <a id="lineup-thursday"></a>
+                    <h3 class="text-irie text-uppercase">
+                        <b>Donderdag (Kortemark Congé) 
+                            <a href="/nl/lineup/kortemark-conge/">
+                                <i class="fas fa-link fa-xs" style="font-size: 0.6rem;"></i>
+                            </a>
+                        </b>
+                    </h3>
+                    <?php include($_SERVER['DOCUMENT_ROOT'].'/src/static/lineup/kortemark-conge.php');?>
+                    <a id="lineup-friday"></a>
+                    <h3 class="text-irie text-uppercase">
+                        <b>Vrijdag 
+                            <a href="/nl/lineup/friday/">
+                                <i class="fas fa-link fa-xs" style="font-size: 0.6rem;"></i>
+                            </a>
+                        </b>
+                    </h3>
+                    <?php include($_SERVER['DOCUMENT_ROOT'].'/src/static/lineup/friday.php');?>
+                    <a id="lineup-saturday"></a>
+                    <h3 class="text-irie text-uppercase">
+                        <b>Zaterdag 
+                            <a href="/nl/lineup/saturday/">
+                                <i class="fas fa-link fa-xs" style="font-size: 0.6rem;"></i>
+                            </a>
+                        </b>
+                    </h3>
+                    <?php include($_SERVER['DOCUMENT_ROOT'].'/src/static/lineup/saturday.php');?>
                 </div>
             </div>
         </div>
