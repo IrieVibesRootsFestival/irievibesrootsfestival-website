04cd76791aa92ae3e16a391c18e718e0663cb07b
Anna <djsndcx@gmail.com>
2019-12-31 14:26:27 +0100

    add text

--- en/index.php
+++ en/index.php
@@ -51,6 +51,7 @@
                 </div>
                 <!-- Aftermovie -->
                 <div class="col-12 mb-md-3">
+                    <h1 class="mb-md-3 mt-2 mt-md-0 text-center quote">Aftermovie</h1>
                     <div id="slider-wrapper" class="rounded p-2 mb-1">
                         <style>.embed-container { position: relative; padding-bottom: 43%; height: 0; overflow: hidden; max-width: 100%; } .embed-container iframe, .embed-container object, .embed-container embed { position: absolute; top: 0; left: 0; width: 100%; height: 100%; }</style><div class='embed-container'><iframe src='https://player.vimeo.com/video/371957649?autoplay=1&portrait=false&muted=true' frameborder='0' webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe></div>
                     </div>
