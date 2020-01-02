c29d2cefec1617e54eb1a978a682717272b18062
Anna <djsndcx@gmail.com>
2020-01-02 19:25:41 +0100

    Change the news page

--- src/static/content/news-links.php
+++ src/static/content/news-links.php
@@ -1,19 +1,29 @@
 <div class="row">
     <!-- Image -->
-    <div class="col-10 col-md-5 ml-4 ml-md-0"><img alt="" src="/src/img/news/reggae-be-irie-vibes-2019-de-tweede-jeugd-van-een-festival.webp" class="img-fluid rounded"/></div>
+    <div class="col-10 col-md-5 ml-4 ml-md-0">
+        <picture>
+            <source srcset="/src/img/news/nieuwsbrief-september-3776469.webp" type="image/webp">
+            <source srcset="/src/img/news/nieuwsbrief-september-3776469.jpg" type="image/jpeg">
+            <img alt="" class="img-fluid rounded" src="/src/img/news/nieuwsbrief-september-3776469.jpg"/>
+        </picture>
+    </div>
+    <!-- Title & Text -->
+    <div class="col-12 col-md-7 mt-2">
+        <a href="/src/img/news/reggae-be-irie-vibes-2019-de-tweede-jeugd-van-een-festival.webp"><h4>Nieuwsbrief </h4></a>
+        <small>De Irie Vibes nieuwsbrief van Januari 2020.</small>
+    </div>
+    <!-- Image -->
+    <div class="col-10 col-md-5 ml-4 ml-md-0">
+        <picture>
+            <source srcset="/src/img/news/reggae-be-irie-vibes-2019-de-tweede-jeugd-van-een-festival.webp" type="image/webp">
+            <source srcset="/src/img/eco/clean-water/clean-water-info.jpg" type="image/jpeg">
+            <img alt="" class="img-fluid rounded" src="/src/img/eco/clean-water/clean-water-infor.jpg"/>
+        </picture>
+    </div>
     <!-- Title & Text -->
     <div class="col-12 col-md-7 mt-2">
         <a href="http://www.reggae.be/nl/news?id=15437&fbclid=IwAR1jj7TppgJhZoKyvBT_IRwkXnpvabAGrOu3pyu-sq9r8-PKZl9dtpzfDEM"><h4>Irie Vibes 2019: De tweede jeugd van een festival</h4></a>
         <small>Terug naar de roots is altijd goed. Op Irie Vibes betekende dat dit jaar terug naar één podium, en daardoor terug naar de eenheid en verbond...</small>
-    </div>      
-</div>
-<div class="row">
-    <!-- Image -->
-    <div class="col-10 col-md-5 ml-4 ml-md-0"><img alt="" src="/src/img/news/nieuwsblad-be-muziek-en-ecologie-drie-dagen-centraal.webp" class="img-fluid rounded"/></div>
-    <!-- Title & Text -->
-    <div class="col-12 col-md-7 mt-2">
-        <a href="https://www.nieuwsblad.be/cnt/dmf20190716_04513998"><h4>Muziek en ecologie drie dagen centraal
-            </h4></a>
-        <small>De achttiende editie van het Irie Vibes Roots Festival vindt vrijdag en zaterdag plaats. Het festival wordt afgetrapt met Kortemark Congé...</small>
-    </div>      
+    </div> 
+          
 </div>
