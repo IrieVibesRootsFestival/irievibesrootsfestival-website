007867b1bfe756aa1227c3d2959b15f35cb5e38b
Anna <djsndcx@gmail.com>
2020-01-05 16:42:10 +0100

    Update thursday.php

--- src/lineup/thursday.php
+++ src/lineup/thursday.php
@@ -1,6 +1,6 @@
 <div id="lineup" class="row text-center">
     <!-- Lion's Den -->
-    <div id="stage-lions-den" class="col-12 col-md-6">
+    <div id="stage-lions-den" class="col-12 col-lg-6">
         <div class="row py-2 mb-3 border border-lg border-success rounded bg-img">
             <div class="col-12"><p class="bg-success text-center rounded text-white">Lion's Den</p></div>
             <div class="col-12"> <!--Change to Col-6 when teaser is over-->
@@ -14,66 +14,48 @@
             </div>
         </div>
     </div>
-</div>
-<!-- Open Air
-<div div="stage_air" class="col-md-6">
-<div class="row py-2 mb-3 border border-lg border-danger rounded bg-img">
-<div class="col-12"><p class="text-center bg-danger rounded text-white">Open Air</p></div>
-</div>
-</div> 
--->
-
-<!-- Little Lion-->
-<div id="wrapper" class="col-md-6 px-0 d-none">
-    <!-- Inner Circle -->
-    <div id="stage_tent_outside" class="col-12">
-        <div class="row py-2 mb-3 border border-lg border-sand rounded bg-img">
-            <div class="col-12"><p class="bg-sand text-center rounded text-white">Inner Circle</p></div>
-            <div class="col-6">
-                <div class="text-white bg-dark px-1 rounded h4">12:00</div>
-                <div class="img-fluid img-container">
-                    <img alt="" class="img-fluid rounded" src="/src/img/artists/sattasound-babbajah.jpg"></div>
-                <a class="d-block text-center pt-2" href="https://www.facebook.com/sattasoundsystem/">Satta Sound ft. Babbajah</a>
-            </div>
-            <div class="col-6">
-                <div class="text-white bg-dark px-1 rounded h4">15:00</div>
-                <div class="img-fluid img-container">
-                    <img alt="" class="img-fluid rounded" src="/src/img/artists/fishermen-sound.jpg">
-                </div>
-                <a class="d-block text-center pt-2" href="https://www.facebook.com/FisherMenDub/">Fishermen Sound</a>
-            </div>
-            <div class="col-6">
-                <div class="text-white bg-dark px-1 rounded h4">17:15</div>
-                <div class="img-fluid img-container">
-                    <img alt="" class="img-fluid rounded" src="/src/img/artists/forward-fever.jpg">
-                </div>
-                <a class="d-block text-center pt-2" href="https://www.facebook.com/forward.fever/">Forward Fever</a>
-            </div>
-            <div class="col-6">
-                <div class="text-white bg-dark px-1 rounded h4">20:00</div>
-                <div class="img-fluid img-container">
-                    <img alt="" class="img-fluid rounded" src="/src/img/artists/earthworks.jpg">
-                </div>
-                <a class="d-block text-center pt-2" href="https://www.earthworksamsterdam.com/">Earthworks</a>
+    <!-- Open Air -->
+    <div id="stage-open-air" class="col-12 col-lg-6">
+        <div class="row py-2 mb-3 border border-lg border-danger rounded bg-img">
+            <div class="col-12"><p class="bg-danger text-center rounded text-white">Open Air</p></div>
+            <div class="col-12"> <!--Change to Col-6 when teaser is over-->
+                <div class="text-white bg-dark px-1 rounded h4">TEASER</div>
+                <picture>
+                    <source srcset="/src/img/artists/.webp" type="image/webp">
+                    <source srcset="/src/img/artists/.jpg" type="image/jpeg">
+                    <img alt="" class="img-fluid rounded img-container" src="/src/img/artists/.jpg"/>
+                </picture>
+                <a class="d-block text-center pt-2" href="#">#</a>
             </div>
         </div>
     </div>
-    <div id="stage_kids" class="col-12">
-        <!-- Kinderdorp -->
-        <div class="row py-2 mb-3 border border-lg border-warning rounded bg-img">
-            <div class="col-12"><p class="bg-warning rounded text-center text-white">Little Lion</p></div>
-            <div class="col-12"><div class="text-white bg-dark p-1 rounded h4">Continuous</div></div>
-            <div class="col-6">
-                <div class="img-fluid img-container">
-                    <img alt="" class="img-fluid rounded" src="/src/img/artists/funny_haircuts.jpg">
-                </div>
-                <a class="d-block pt-2" href="">Crazy Hairstyles</a>
+    <!-- Inner Circle -->
+    <div id="stage-inner-circle" class="col-12 col-lg-6">
+        <div class="row py-2 mb-3 border border-lg border-sand rounded bg-img">
+            <div class="col-12"><p class="bg-sand text-center rounded text-white">Inner Circle</p></div>
+            <div class="col-12"> <!--Change to Col-6 when teaser is over-->
+                <div class="text-white bg-dark px-1 rounded h4">TEASER</div>
+                <picture>
+                    <source srcset="/src/img/artists/.webp" type="image/webp">
+                    <source srcset="/src/img/artists/.jpg" type="image/jpeg">
+                    <img alt="" class="img-fluid rounded img-container" src="/src/img/artists/.jpg"/>
+                </picture>
+                <a class="d-block text-center pt-2" href="#">#</a>
             </div>
-            <div class="col-6">
-                <div class="img-fluid img-container">
-                    <img alt="" class="img-fluid rounded" src="/src/img/artists/making_jewelry.jpg"> 
-                </div>
-                <a class="d-block pt-2" href="">Making Jewelry</a>                
+        </div>
+    </div>
+    <!-- Inner Circle -->
+    <div id="stage-little-lion" class="col-12 col-lg-6">
+        <div class="row py-2 mb-3 border border-lg border-warning rounded bg-img">
+            <div class="col-12"><p class="bg-warning text-center rounded text-white">Little Lion</p></div>
+            <div class="col-12"> <!--Change to Col-6 when teaser is over-->
+                <div class="text-white bg-dark px-1 rounded h4">TEASER</div>
+                <picture>
+                    <source srcset="/src/img/artists/.webp" type="image/webp">
+                    <source srcset="/src/img/artists/.jpg" type="image/jpeg">
+                    <img alt="" class="img-fluid rounded img-container" src="/src/img/artists/.jpg"/>
+                </picture>
+                <a class="d-block text-center pt-2" href="#">#</a>
             </div>
         </div>
     </div>
