c50fb607f143d7b7c44b2f354362feee997c47c4
Anna <djsndcx@gmail.com>
2020-01-12 18:32:10 +0100

    Add changes to the festival rules pages

--- en/festival/rules/index.php
+++ en/festival/rules/index.php
@@ -13,24 +13,23 @@
             <div class="row">
                 <h3 class="text-irie text-uppercase col-12"><b>Festival rules</b></h3>
                 <div class="col-12 col-md-6">
-                    <p class="text-danger">What is <b>NOT</b> allowed?</p>
+                    <h4 class="text-danger">What is <b>NOT</b> allowed?</h4>
                     <ul>
                         <li>glass</li>
                         <li>weapons</li> 
-                        <li>animals</li> 
+                        <li>animals <i>(we make an exception for guide- and assitance dogs)</i></li> 
                         <li>sharp items</li>
-                        <li>open fire</li>
-                        <li>climbing on fences</li>
-                        <li>access to the backstage</li>
-                        <li>access to the stage</li>
+                        <li>open fire or barbecue pits</li>
+                        <li>climbing on fencing and railing</li>
+                        <li>access to the (back)stage</li>
                         <li>bringing your own drinks to the festival</li>
-                        <li>creating your own party on the camping (no sounds!)</li>
+                        <li>bringing your own soundsystem to the camping</li>
                     </ul>
                 </div>
                 <div class="col-12 col-md-6">
-                    <p class="text-success">What is allowed?</p>
+                    <h4 class="text-success">What is allowed?</h4>
                     <ul>
-                        <li>Empty reusable bottles to fill up with water at the waterbar (only clear plastic ones!)</li>
+                        <li>empty reusable bottles to fill with water at the water bar (only clear plastic ones!)</li>
                         <li>Utilities to take care of children (diapers, drinking bottles,...)</li>
                     </ul>
                 </div>
@@ -38,14 +37,14 @@
                 <h3 class="text-irie text-uppercase col-12"><b>Facilities</b></h3>
                 <div class="col-12">
                     <ul>
-                        <li><b>Info Point</b>: ready to help you out with all kinds of questions about safety at Irie Vibes.  You can also get free earplugs, sunscreen for the children, Irie Vibes tshirts, gadgets and pocket ash trays.</li>
+                        <li><b>Info Point</b>: the beating heart of the festival, ready to help you out with all kinds of questions. Visitors, sponsors, guests and shops can address the info point. You can also get free earplugs, ear protection for children, pocket ashtrays and gadgets.</li>
                         <li><b>Toilets</b>:  €1 to pee all weekend! It is not allowed to pee outside the toilet area!</li>
-                        <li><b>Ecoteam</b>: Be nice to our eco-team and mother nature! Please throw your garbage in the garbage bins!</li>
-                        <li><b>Safe ‘n Sound</b>: This information booth is a cooperation with VAD. They will provide you with information about alcohol, drugs, ear damage and safe sex. Condoms can be obtained at this shop.</li>
-                        <li><b>Food Stands + Bar</b>: There will be plenty food stands available and 3 bars (main bar, coffee/cocktail bar and camping bar)</li>
-                        <li><b>Camping</b>: All information about accomodation can be found at the website</li>
-                        <li><b>Parking</b>: Cars can not stay on the camping. We ask you to park your car on the parking site. You will pay only 5 euros for the whole weekend.</li>
-                        <li><b>First Aid</b>: There is a first aid tent, where they will help you out for all kind of injures.</li>
+                        <li><b>Ecoteam</b>: Respect nature and our ecoteam. Leave your waste in the proper garbage bins. Gather your garbage at the camping in a bag and bring your bags to the entrance on Sunday morning in exchange for a coffee. Ask for your pocket ashtray at the info point.</li>
+                        <li><b>Safe ‘n Sound</b>: This information booth is a cooperation with VAD. They will provide you with information about alcohol, drugs, ear damage and safe sex.</li>
+                        <li><b>Food Stands + Bar</b>: There will be plenty of food stands available and 3 bars (main bar, coffee/cocktail bar and camping bar)</li>
+                        <li><b>Camping</b>: All information about accomodation can be found on the website</li>
+                        <li><b>Parking</b>: Cars aren't allowed on the camping. We ask you to park your car on the parking site. A weekend pass costs €5. Carpooling is a great alternative to driving your own car. There will be hourly shuttle busses between the festival terrain and train station, to make sure you're never left out.</li>
+                        <li><b>First Aid</b>: The first aid tent provided by the Red Cross of Belgium is there to soothe all injuries you have. Don't hesitate to ask for help!</li>
                     </ul>
                 </div>
             </div>
--- nl/festival/rules/index.php
+++ nl/festival/rules/index.php
@@ -19,32 +19,31 @@
                         <li>wapens</li> 
                         <li>dieren (met uitzondering tot begeleidende dieren)</li> 
                         <li>scherpe voorwerpen</li>
-                        <li>open vuur</li>
+                        <li>open vuur of eigen barbecue toestellen</li>
                         <li>over de hekken klimmen</li>
                         <li>toegang tot de backstage ruimte</li>
                         <li>toegang tot het podium</li>
                         <li>eigen drank meenemen</li>
-                        <li>je eigen feestje bouwen op de camping (geen sounds!)</li>
+                        <li>je eigen soundsystem mee nemen naar de camping</li>
                     </ul>
                 </div>
                 <div class="col-12 col-md-6">
                     <p class="text-success"><span class="text-box rounded">Dit is toegelaten:</span></p>
                     <ul>
-                        <li>Een hervulbare fles<br/>
-                            <small><i>Je kan je fles vullen met water aan het waterpunt. Het is gratis! (Alleen transparante plastic flessen.)</i></small>
-                        </li>
+                        <li>een hervulbare fles: Je kan je fles vullen met water aan het waterpunt. Het is gratis! (Alleen transparante plastic flessen.)</li>
+                        <li>eigen voorziene producten om je baby of kind te verzorgen (luiers, drinkfles, ...)</li>
                     </ul>
                 </div>
             </div>
             <h3 class="text-white"><b>Faciliteiten</b></h3>
             <ul>
-                <li><b>Infopunt:</b>: Het Infopunt staat klaar om te helpen met vragen over de veiligheid op Irie Vibes. Je kan er ook gehoorbescherming, Irie Vibes T-shirts, zak-asbakjes, gadgets en zonnecrème voor kinderen krijgen</li>
-                <li><b>Toiletten</b>: Het kost €1 om het hele weekend het toilet te gebruiken. Het is niet toegelaten u te behoeften buiten de toiletten.</li>
-                <li><b>Ecoteam</b>: Wees vriendelijk voor ons eco-team. Gooi je afval in de vuilnisbak!</li>
-                <li><b>Food Stands + Bar</b>: Er is voldoende keuze in voedselkraampjes voorzien. Er zijn drie bars voorzien: de camping bar (Chiro), de cocktail-bar en de main-bar.
+                <li><b>Infopunt:</b>: Het kloppend hart van het festival, klaar om een antwoord te geven op al je vragen. Bezoekers, sponsors, gasten en shops kunnen hier terecht. Je kan er ook gratis oordopjes verkrijgen, gehoorbescherming voor kinderen, zakasbakjes en gadgets.</li>
+                <li><b>Toiletten</b>: Het kost €1 om het hele weekend het toilet te gebruiken.</li>
+                <li><b>Ecoteam</b>: Toon respect voor de natuur en ons ecoteam. Gooi je afval in de juiste vuilnisbakken. Verzamel je afval op de camping in een vuilniszak en brengt deze op zondagmorgen naar de ingang in ruil voor een koffie. Vraag naar je zakasbakje aan het infopunt.</li>
+                <li><b>Food Stands + Bar</b>: Op onze markt vind je een ruim en gevarieerd aanbod foodtrucks. Voor een drankje kan je terecht aan de camping bar (Chiro), de cocktail-bar en de main-bar.
                 <li><b>Camping</b>: Informatie over de camping kan je <a href="/nl/festival/accomodation/">hier</a> vinden.</li>
-                <li><b>Parking</b>: Cars can not stay on the camping. We ask you to park your car on the parking site. You will pay only 5 euros for the whole weekend.</li>
-                <li><b>EHBO</b>: There is a first aid tent, where they will help you out for all kind of injures.</li>
+                <li><b>Parking</b>: Auto’s zijn niet toegelaten op de camping. Je kan je wagen parkeren op het dichtbij gelegen parkeerterrein. Je betaalt 5 euro voor het hele weekend. Probeer om te carpoolen of met het openbaar vervoer te komen. Elk uur is er een gratis shuttlebus van het station naar de festivalweide.</li>
+                <li><b>EHBO</b>: Er is een EHBO tent bemand door de vrijwilligers van het Rode Kruis. Zij zijn permanent op het festivalterrein aanwezig.</li>
             </ul>
         </div>
         <!-- Include Footer -->
