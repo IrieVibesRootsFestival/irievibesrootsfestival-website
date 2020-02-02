62f751d0d9a806e72e74a68a6befae460fd9e81a
Anna <djsndcx@gmail.com>
2020-02-02 22:19:10 +0100

    Update header_en.php

--- src/static/pages/header_en.php
+++ src/static/pages/header_en.php
@@ -3,13 +3,13 @@
 <header>
     <div class="text-center my-3 row" id="banner-date">
         <!--Hashtag info-->
-        <div class="col-12 col-lg-3">
+        <div class="col-12 col-lg-4">
             <p class="text-white m-0"><i>#worldmarkets #ital #roots</i></p></div>
         <!--Main middle info-->
-        <div class="col-12 col-lg-3">
+        <div class="col-12 col-lg-4">
             <p class="text-white text-uppercase m-0"><b> 16 - 17 - 18 July 2020 // Thursday free entry</b></p></div>
         <!--Hashtag info-->
-        <div class="col-12 col-lg-3">
+        <div class="col-12 col-lg-4">
             <p class="text-white m-0"><i>#kids #family #reggae #dub</i></p>
         </div>
     </div>
