d2b2a4b5a20ac430e6e774209ca6b092fd766a9a
KiddieD <djsndcx@gmail.com>
2020-02-29 23:19:39 +0100

    Add The Resonators to the lineup

--- src/lineup/saturday.php
+++ src/lineup/saturday.php
@@ -3,12 +3,21 @@
     <div id="stage-lions-den" class="col-12 col-lg-6">
         <div class="row py-2 mb-3 border border-lg border-success rounded bg-img">
             <div class="col-12"><p class="text-center bg-success rounded text-white">Lion's Den</p></div>
-            <div class="col-12">
+            <div class="col-6">
                 <div class="text-white bg-dark px-1 rounded h4">TEASER</div>
                 <div class="facebook-responsive">
                     <iframe src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fwatch%2F%3Fv%3D1062072777464176&width=500&show_text=false&height=280&appId" width="" height="" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media" allowFullScreen="true"></iframe>
                 </div>
                 <a class="d-block text-center pt-2" href="https://www.facebook.com/Spellbreakersband">Spellbreakers</a>
+            </div>
+            <div class="col-6">
+                <div class="text-white bg-dark px-1 rounded h4">TEASER</div>
+                <picture>
+                    <source srcset="/src/img/artists/the-resonators.webp" type="image/webp">
+                    <source srcset="/src/img/artists/the-resonators.jpg" type="image/jpeg">
+                    <img alt="" class="img-fluid rounded" src="/src/img/artists/the-resonators.jpg"/>
+                </picture>
+                <a class="d-block text-center pt-2" href="https://www.facebook.com/Resonators/">The Resonators</a>
             </div>
         </div>
     </div>
