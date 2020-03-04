f82d104172f9569685ff06be177d1c571be70b81
KiddieD <djsndcx@gmail.com>
2020-03-04 16:37:45 +0100

    Update index.php

--- en/artists/index.php
+++ en/artists/index.php
@@ -1,15 +1,16 @@
-<!DOCTYPE html><html lang="nl">
+<!DOCTYPE html>
+<html lang="en">
     <head>
         <!-- Import meta tags, OpenGraphs, stylesheets and common includes -->
         <?php include($_SERVER['DOCUMENT_ROOT'].'/src/static/pages/head_common.php');?>
         <title>Artiesten - Irie Vibes</title>
         <meta property="og:title" content="Artists"/>
-        <meta property="og:description" content="Wie speelt op Irie Vibes dit jaar? Je vindt het hier!" />
+        <meta property="og:description" content="Who's playing on Irie Vibes this year? Find out here!" />
     </head>
     <body>
-        <?php include($_SERVER['DOCUMENT_ROOT'].'/src/static/pages/header_nl.php');?>
+        <?php include($_SERVER['DOCUMENT_ROOT'].'/src/static/pages/header_en.php');?>
         <div id="artists" class="container content">
-            <h3 class="display-7 text-center">Artiesten</h3>
+            <h3 class="display-7 text-center">Artists</h3>
             <div class="row">
                 <div class="col-12 px-3 text-center">
                     <div class="btn-group mb-3" role="group" aria-label="Nav-button">
@@ -39,7 +40,7 @@
                                         <!--Artist text-->
                                         <div class="col-12 col-md-9"> 
                                             <h3>Jungle Roots</h3>
-                                            <p><b>Jungle Roots</b> uit Antwerpen bestaat uit vier vaste bandleden die er van in het begin bij waren, aangevuld door onze vaste blazerssectie (Jungle Dub Warriors).<br/>Ze  spelen voornamelijk Roots Reggae met verschillende andere muzikale invloeden om een groter publiek buiten Reggae liefhebbers aan te trekken.<br/><i>“Reggae music isn't something you hear it's something that you feel”</i></p>
+                                            <p>...</p>
                                         </div>
                                     </div>
                                 </div>
@@ -58,7 +59,7 @@
                                         <!--Artist text-->
                                         <div class="col-12 col-md-9"> 
                                             <h3>The Resonators</h3>
-                                            <p>Geleid door dual vocal powerhouse Faye Houstan en Kassia Zermon, neemt de negenkoppige band The Resonators ons mee op een geluids excursie die zich uitstrekt van licht en soulful, naar donker en gruizig.<br/>Tien jaar na hun start willen ze nog steeds iets positiefs in de wereld brengen.<br/>Door het formidabele vakmanschap van dub-ingenieur Darren Jamtone aan de besturing, bieden Resonators een krachtige, psychedelische en euforische ervaring, zowel op plaat als op het podium.  Bekendheid verwierven ze door hun geweldige liveshows en hun support acts voor grootheden zoals The Skatalites, Lee Scratch Perry, Jimmy Cliff, Fat Freddy’s Drop en Horace Andy om er maar een paar te noemen.<br/>Hun boodschap blijft er een van optimisme en het vermogen te overwinnen door liefde en groei.<br/><b>The Resonators, Zaterdag 18th July, Lion’s Den</b><br/><a href="https://www.youtube.com/watch?v=l52etcQJrO4">The Resonators op Youtube</a></p>
+                                            <p>...</p>
                                         </div>
                                     </div>
                                 </div>
@@ -98,7 +99,7 @@
                                         <!--Artist text-->
                                         <div class="col-12 col-md-9"> 
                                             <h3>Oxala Tamtam</h3>
-                                            <p>Warmte, gastvrijheid, ritmes, vibes... ben je er klaar voor? Wil je je helemaal laten gaan op de opzwepende ritmes van <b>Oxala Tamtam</b>? <br/>Dat kan op donderdag tot twee keer toe: om 20u in de Lion’s Den en om 23u op het terrein.</p>
+                                            <p>...</p>
                                         </div>
                                     </div>
                                 </div>
@@ -119,7 +120,7 @@
                                         <!--Artist text-->
                                         <div class="col-12 col-md-9"> 
                                             <h3>Bee & Health</h3>
-                                            <p>Honingbijen vormen een essentiële rol in de ecologische kringloop van onze leefwereld.<br/>Het is dan ook van groot belang om de bijen effectief te beschermen en ons er bewust van te zijn hoe onze manier van leven een mogelijke oorzaak kan vormen van de bijensterfte.<br/>In de voordracht: “Het belang van bijen en bijenproducten” tracht jonge lokale imker Robbe Dhulster uit te leggen hoe een bijenvolk in elkaar zit. Hoe komt dat de bijenpopulatie het jaar na jaar steeds moeilijker krijgt om te overleven ?  Wat is hiervoor de oorzaak ? En wat kunnen wij als mens doen?<br/>Gedurende het festival vind je Robbe ook terug met zijn stand Bee & Health op het festivalterrein.</p>
+                                            <p>...</p>
                                         </div>
                                     </div>
                                 </div>
@@ -133,7 +134,7 @@
             </div>
         </div>
         <!-- Include Footer -->
-        <?php include($_SERVER['DOCUMENT_ROOT'].'/src/static/pages/footer_nl.php');?>        
+        <?php include($_SERVER['DOCUMENT_ROOT'].'/src/static/pages/footer_en.php');?>        
     </body>
     <!--Loading bootstrap and jQuery scripts at the end of the page to prevent slowdowns-->
     <?php include($_SERVER['DOCUMENT_ROOT'].'/src/static/pages/load_last.php');?>
