785a557d09bb3a8f7ada988fac753fe9a82092f0
KiddieD <djsndcx@gmail.com>
2020-05-29 10:44:44 +0200

    new news item

--- src/static/content/news-links.php
+++ src/static/content/news-links.php
@@ -1,5 +1,18 @@
 <div class="row">
     <!-- Image -->
+    <div class="col-12 col-md-4 text-center">
+        <picture>
+            <source srcset="/src/img/news/irie-vibes-trui-webshop-advertentie.webp" type="image/webp">
+            <source srcset="/src/img/news/irie-vibes-trui-webshop-advertentie.jpg" type="image/jpeg">
+            <img alt="" class="img-fluid rounded w-80" src="/src/img/news/irie-vibes-trui-webshop-advertentie.jpg"/>
+        </picture>
+    </div>
+    <!-- Title & Text -->
+    <div class="col-12 col-md-8 mt-2">
+        <a href="https://shop.irievibes.be"><h4>De Irie Vibes webshop</h4></a>
+        <small>Je kan nu ook je favoriete items van het Irie Vibes Roots festival op onze webshop kopen. Koop je lidkaart vandaag, en geniet van 10% korting op de webshop en onze evenementen!</small>
+    </div> 
+    <!-- Image -->
     <div class="col-12 col-md-4 mb-1 text-center">
         <picture>
             <source srcset="/src/img/news/nieuwsbrief-februari-4141742.webp" type="image/webp">
@@ -11,19 +24,5 @@
     <div class="col-12 col-md-8 mt-2">
         <a href="https://mailchi.mp/e1c3fd6c8a65/nieuwsbrief-februari-4141742"><h4>Nieuwsbrief Maart</h4></a>
         <small>Meer info over Dub It Forward, Positivity Weekend foto's, ...</small>
-    </div>
-    <!-- Image -->
-    <div class="col-12 col-md-4 text-center">
-        <picture>
-            <source srcset="/src/img/news/reggae-be-irie-vibes-2019-de-tweede-jeugd-van-een-festival.webp" type="image/webp">
-            <source srcset="/src/img/eco/clean-water/clean-water-info.jpg" type="image/jpeg">
-            <img alt="" class="img-fluid rounded w-80" src="/src/img/eco/clean-water/clean-water-infor.jpg"/>
-        </picture>
-    </div>
-    <!-- Title & Text -->
-    <div class="col-12 col-md-8 mt-2">
-        <a href="http://www.reggae.be/nl/news?id=15437&fbclid=IwAR1jj7TppgJhZoKyvBT_IRwkXnpvabAGrOu3pyu-sq9r8-PKZl9dtpzfDEM"><h4>Irie Vibes 2019: De tweede jeugd van een festival</h4></a>
-        <small>Terug naar de roots is altijd goed. Op Irie Vibes betekende dat dit jaar terug naar één podium, en daardoor terug naar de eenheid en verbond...</small>
-    </div> 
-          
+    </div>          
 </div>
