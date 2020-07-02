41290b1faf4045c5827c11e04255950dbaaa91c4
KiddieD <djsndcx@gmail.com>
2020-07-02 11:28:22 +0200

    Update index.php

--- nl/info/eco/sdg/index.php
+++ nl/info/eco/sdg/index.php
@@ -10,7 +10,7 @@
     <body>
         <?php include($_SERVER['DOCUMENT_ROOT'].'/src/static/pages/header_nl.php');?>
         <div class="container content">         
-            <h1 style="margin-bottom:5px" class="display-7 text-center">Sustainable Development Goals</h1>
+            <h1 style="margin-bottom:5px" class="display-7 text-center">Duurzame Ontwikkelingsdoelstellingen</h1>
             <br/>
             <div class="row">
                 <!-- Main text -->
