3a8fc200e480e566d46ca0fe0e1d21a1e369d110
KiddieD <djsndcx@gmail.com>
2020-07-12 19:04:10 +0200

    Update index.php

--- en/contact/index.php
+++ en/contact/index.php
@@ -14,63 +14,75 @@
             <div class="row text-irie">
                 <div class="row">
                     <div class="col-lg-4 col-md-4 col-sm-4 col-12">
-                        <div class="p-2 m-1 text-box text-center">
-                            <i class="fa fa-instagram fa-3x" aria-hidden="true"></i>
-                            <div class="h4">
-                                <h4>Info</h4>
+                        <div class="text-box-2 p1">
+                            <div class="p-2 m-1 text-box text-center">
+                                <i class="far fa-question-circle h1"></i>
+                                <div class="h3">
+                                    <h3>Info</h3>
+                                </div>
+                                <p></p>
+                                <a href="#">Learn More</a>
                             </div>
-                            <p></p>
-                            <a href="#">Learn More</a>
                         </div>
                     </div>
                     <div class="col-lg-4 col-md-4 col-sm-4 col-12">
-                        <div class="p-2 m-1 text-box text-center">
-                            <i class="fa fa-twitter fa-3x" aria-hidden="true"></i>
-                            <div class="h4">
-                                <h4>Promo</h4>
+                        <div class="text-box-2 p1">
+                            <div class="p-2 m-1 text-box text-center">
+                                <i class="fas fa-share-alt-square h1"></i>
+                                <div class="h3">
+                                    <h3>Promo</h3>
+                                </div>
+                                <p>for all questions about the webshop or membership cards</p>
+                                <a href="#">Learn More</a>
                             </div>
-                            <p></p>
-                            <a href="#">Learn More</a>
                         </div>
                     </div>
                     <div class="col-lg-4 col-md-4 col-sm-4 col-12">
-                        <div class="p-2 m-1 text-box text-center">
-                            <i class="fa fa-facebook fa-3x" aria-hidden="true"></i>
-                            <div class="h4">
-                                <h4>Finance</h4>
+                        <div class="text-box-2 p1">
+                            <div class="p-2 m-1 text-box text-center">
+                                <i class="fa fa-facebook fa-3x" aria-hidden="true"></i>
+                                <div class="h3">
+                                    <h3>Finance</h3>
+                                </div>
+                                <p></p>
+                                <a href="#">Learn More</a>
                             </div>
-                            <p></p>
-                            <a href="#">Learn More</a>
                         </div>
                     </div>
                     <div class="col-lg-4 col-md-4 col-sm-4 col-12">
-                        <div class="p-2 m-1 text-box text-center">
-                            <i class="fa fa-pinterest-p fa-3x" aria-hidden="true"></i>
-                            <div class="h4">
-                                <h4>Shops</h4>
+                        <div class="text-box-2 p1">
+                            <div class="p-2 m-1 text-box text-center">
+                                <i class="fa fa-pinterest-p fa-3x" aria-hidden="true"></i>
+                                <div class="h3">
+                                    <h3>Shops</h3>
+                                </div>
+                                <p></p>
+                                <a href="#">Learn More</a>
                             </div>
-                            <p></p>
-                            <a href="#">Learn More</a>
                         </div>
                     </div>
                     <div class="col-lg-4 col-md-4 col-sm-4 col-12">
-                        <div class="p-2 m-1 text-box text-center">
-                            <i class="fas fa-eye h1"></i>
-                            <div class="h4">
-                                <h4>Security</h4>
+                        <div class="text-box-2 p1">
+                            <div class="p-2 m-1 text-box text-center">
+                                <i class="fas fa-eye h1"></i>
+                                <div class="h3">
+                                    <h3>Security</h3>
+                                </div>
+                                <p>for all information about safety at our events or if you want to be part of team security at Irie Vibes Roots Festival</p>
+                                <a href="mailto:security@irievibes.be">security@irievibes.be</a>
                             </div>
-                            <p>for all information about safety at our events or if you want to be part of team security at Irie Vibes Roots Festival</p>
-                            <a href="mailto:security@irievibes.be">security@irievibes.be</a>
                         </div>
                     </div>
                     <div class="col-lg-4 col-md-4 col-sm-4 col-12">
-                        <div class="p-2 m-1 text-box text-center">
-                            <i class="fas fa-mouse-pointer h1"></i>
-                            <div class="h4">
-                                <h4>Webmaster</h4>
+                        <div class="text-box-2 p1">
+                            <div class="p-2 m-1 text-box text-center">
+                                <i class="fas fa-mouse-pointer h1"></i>
+                                <div class="h3">
+                                    <h3>Webmaster</h3>
+                                </div>
+                                <p>Found an error on the website? Interested in helping us out? We're more than happy to talk to you.</p>
+                                <a href="mailto:web@irievibes.be">web@irievibes.be</a>
                             </div>
-                           <p>Found an error on the website? Interested in helping us out? We're more than happy to talk to you.</p>
-                            <a href="mailto:web@irievibes.be">web@irievibes.be</a>
                         </div>
                     </div>
                 </div>
