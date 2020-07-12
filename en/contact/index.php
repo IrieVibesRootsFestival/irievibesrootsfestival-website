291d583a9fb24ea1c04cdfc89811c2b7af0b324c
KiddieD <djsndcx@gmail.com>
2020-07-12 19:00:17 +0200

    Update index.php

--- en/contact/index.php
+++ en/contact/index.php
@@ -16,72 +16,60 @@
                     <div class="col-lg-4 col-md-4 col-sm-4 col-12">
                         <div class="p-2 m-1 text-box text-center">
                             <i class="fa fa-instagram fa-3x" aria-hidden="true"></i>
-                            <div class="title">
+                            <div class="h4">
                                 <h4>Info</h4>
                             </div>
-                            <div class="text">
-                                <span>Lorem ipsum dolor sit amet, id quo eruditi eloquentiam. Assum decore te sed. Elitr scripta ocurreret qui ad.</span>
-                            </div>
+                            <p></p>
                             <a href="#">Learn More</a>
                         </div>
                     </div>
                     <div class="col-lg-4 col-md-4 col-sm-4 col-12">
                         <div class="p-2 m-1 text-box text-center">
                             <i class="fa fa-twitter fa-3x" aria-hidden="true"></i>
-                            <div class="title">
+                            <div class="h4">
                                 <h4>Promo</h4>
                             </div>
-                            <div class="text">
-                                <span>Lorem ipsum dolor sit amet, id quo eruditi eloquentiam. Assum decore te sed. Elitr scripta ocurreret qui ad.</span>
-                            </div>
+                            <p></p>
                             <a href="#">Learn More</a>
                         </div>
                     </div>
                     <div class="col-lg-4 col-md-4 col-sm-4 col-12">
                         <div class="p-2 m-1 text-box text-center">
                             <i class="fa fa-facebook fa-3x" aria-hidden="true"></i>
-                            <div class="title">
+                            <div class="h4">
                                 <h4>Finance</h4>
                             </div>
-                            <div class="text">
-                                <span>Lorem ipsum dolor sit amet, id quo eruditi eloquentiam. Assum decore te sed. Elitr scripta ocurreret qui ad.</span>
-                            </div>
+                            <p></p>
                             <a href="#">Learn More</a>
                         </div>
                     </div>
                     <div class="col-lg-4 col-md-4 col-sm-4 col-12">
                         <div class="p-2 m-1 text-box text-center">
                             <i class="fa fa-pinterest-p fa-3x" aria-hidden="true"></i>
-                            <div class="title">
+                            <div class="h4">
                                 <h4>Shops</h4>
                             </div>
-                            <div class="text">
-                                <span>Lorem ipsum dolor sit amet, id quo eruditi eloquentiam. Assum decore te sed. Elitr scripta ocurreret qui ad.</span>
-                            </div>
+                            <p></p>
                             <a href="#">Learn More</a>
                         </div>
                     </div>
                     <div class="col-lg-4 col-md-4 col-sm-4 col-12">
                         <div class="p-2 m-1 text-box text-center">
                             <i class="fas fa-eye h1"></i>
-                            <div class="title">
+                            <div class="h4">
                                 <h4>Security</h4>
                             </div>
-                            <div class="text">
-                                <span>for all information about safety at our events or if you want to be part of team security at Irie Vibes Roots Festival</span>
-                            </div>
+                            <p>for all information about safety at our events or if you want to be part of team security at Irie Vibes Roots Festival</p>
                             <a href="mailto:security@irievibes.be">security@irievibes.be</a>
                         </div>
                     </div>
                     <div class="col-lg-4 col-md-4 col-sm-4 col-12">
                         <div class="p-2 m-1 text-box text-center">
                             <i class="fas fa-mouse-pointer h1"></i>
-                            <div class="title">
+                            <div class="h4">
                                 <h4>Webmaster</h4>
                             </div>
-                            <div class="text">
-                                <span>Found an error on the website? Interested in helping us out? We're more than happy to talk to you.</span>
-                            </div>
+                           <p>Found an error on the website? Interested in helping us out? We're more than happy to talk to you.</p>
                             <a href="mailto:web@irievibes.be">web@irievibes.be</a>
                         </div>
                     </div>
