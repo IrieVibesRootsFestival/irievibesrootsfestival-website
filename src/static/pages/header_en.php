2a3e21fa33551ce2ec74d498c3a13c1e09fc7eba
KiddieD <djsndcx@gmail.com>
2020-08-01 22:02:52 +0200

    add icons

--- src/static/pages/header_en.php
+++ src/static/pages/header_en.php
@@ -73,18 +73,18 @@
                         <a href="#" class=" nav-link" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><i class="fab fa-envira"></i>&nbsp;Projects<span class="dropdown-toggle"></span></a>
                         <ul class="dropdown-menu">
                             <li class="dropdown-header">On the festival</li>
-                            <li><a class="nav-link drop-2" href="/en/info/eco/upcycling/">Upcycling</a></li>
-                            <li><a class="nav-link drop-2" href="/en/info/eco/eco-team/">Eco team</a></li>
-                            <li><a class="nav-link drop-2" href="/en/info/eco/reusable-cups/">Reusable cups</a></li>
-                            <li><a class="nav-link drop-2" href="/en/info/eco/groene-vent-award/"><i>Groene Vent</i></a></li>
+                            <li><a class="nav-link drop-2" href="/en/info/eco/upcycling/"><i class="fas fa-recycle"></i> Upcycling</a></li>
+                            <li><a class="nav-link drop-2" href="/en/info/eco/eco-team/"><i class="fas fa-leaf"></i> Eco team</a></li>
+                            <li><a class="nav-link drop-2" href="/en/info/eco/reusable-cups/"><i class="fas fa-glass-whiskey"></i> Reusable cups</a></li>
+                            <li><a class="nav-link drop-2" href="/en/info/eco/groene-vent-award/"><i><i class="fas fa-award"></i> Groene Vent</i></a></li>
                             <li><a class="nav-link drop-2" href="/en/info/eco/clean-water/"><i class="fas fa-hand-holding-water"></i> Clean water</a></li>
-                            <li><a class="nav-link drop-2" href="/en/info/eco/bewilg-het-landschap/">Bewilg Het Landschap</a></li>
+                            <li><a class="nav-link drop-2" href="/en/info/eco/bewilg-het-landschap/"><i class="fas fa-tree"></i> Bewilg Het Landschap</a></li>
                             <div class="dropdown-divider"></div>
                             <li class="dropdown-header">International</li>
-                            <li><a class="nav-link drop-2" href="/en/info/eco/sdg/"><strong>S</strong>ustainable <strong>D</strong>evelopment <strong>G</strong>oals</a></li>
-                            <li><a class="nav-link drop-2" href="/en/info/eco/trim-the-footprint/">Trim The Footprint</a></li>
-                            <li><a class="nav-link drop-2" href="/en/info/eco/world-cleanup-day/">World Cleanup Day</a></li>                        
-                            <li><a class="nav-link drop-2" href="/en/info/eco/outreaching/">Outreaching</a></li>
+                            <li><a class="nav-link drop-2" href="/en/info/eco/sdg/"><i class="fas fa-exchange-alt"></i> <strong>S</strong>ustainable <strong>D</strong>evelopment <strong>G</strong>oals</a></li>
+                            <li><a class="nav-link drop-2" href="/en/info/eco/trim-the-footprint/"><i class="fas fa-exchange-alt"></i> Trim The Footprint</a></li>
+                            <li><a class="nav-link drop-2" href="/en/info/eco/world-cleanup-day/"><i class="fas fa-globe"></i> World Cleanup Day</a></li>                        
+                            <li><a class="nav-link drop-2" href="/en/info/eco/outreaching/"><i class="fas fa-hands"></i> Outreaching</a></li>
                         </ul>
                     </li>
                     <li class="nav-item">
