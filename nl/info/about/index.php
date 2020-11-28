327c4d641179f91071cce8a48ca55770a97c1832
KiddieD <djsndcx@gmail.com>
2020-11-28 20:02:41 +0100

    TinyCME test

---
+++ nl/info/about/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="nl">
+    <head>
+        <!-- Import meta tags, OpenGraphs, stylesheets and common includes -->
+        <?php include($_SERVER['DOCUMENT_ROOT'].'/src/static/pages/head_common.php');?>
+        <title>Over Irie Vibes - Irie Vibes</title>
+        <meta property="og:title" content="Over Irie Vibes"/>
+        <meta property="og:description" content="Over het Irie Vibes Roots Festival" />
+    </head> 
+    <body>
+        <?php include($_SERVER['DOCUMENT_ROOT'].'/src/static/pages/header_nl.php');?>
+        <div class="container content">
+            <textarea id="CMS"></textarea>
+        </div>
+        <!-- Include Footer -->
+        <?php include($_SERVER['DOCUMENT_ROOT'].'/src/static/pages/footer_nl.php');?>        
+    </body>
+    <!--Loading bootstrap and jQuery scripts at the end of the page to prevent slowdowns-->
+    <?php include($_SERVER['DOCUMENT_ROOT'].'/src/static/pages/load_last.php');?>
+</html>
