463365483ab8179d1c3617e1a9a13c69e6185ab2
KiddieD <djsndcx@gmail.com>
2020-11-28 20:02:52 +0100

    Simplification trials

---
+++ en/simplify.php
@@ -0,0 +1,23 @@
+<style>.box.wood{
+    box-shadow: 2px 2px 8px 0 rgba(0,0,0,.2);
+    border:10px solid;
+    border-image-slice:28 27 34 27;
+    border-image-width:10px;
+    border-image-outset:0;
+    border-image-repeat:stretch repeat;
+    border-image-source: url(/src/img/border/hardwood.jpg);
+    background-image: url(/src/img/background/paper.jpg);
+    padding:10px;
+    }
+</style>
+
+<div class="box wood">
+    <h1>Tasty Cookies</h1>
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sed ligula a lacus dignissim rutrum. Phasellus vel orci fermentum nisi euismod elementum vitae eget nisl. Proin et mollis diam, a aliquam odio. Pellentesque vel porttitor urna. Nunc ornare efficitur nisl id viverra. Sed nec enim sollicitudin, cursus lacus eget, gravida dui. Aenean at scelerisque ex. Vivamus eu tristique urna.
+    </p>
+    <h1 style="text-align:right">Yummy cake</h1>
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sed ligula a lacus dignissim rutrum. Phasellus vel orci fermentum nisi euismod elementum vitae eget nisl. Proin et mollis diam, a aliquam odio. Pellentesque vel porttitor urna. Nunc ornare efficitur nisl id viverra. Sed nec enim sollicitudin, cursus lacus eget, gravida dui. Aenean at scelerisque ex. Vivamus eu tristique urna.
+    </p>
+</div>
