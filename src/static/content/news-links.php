d14a846ec2f4c9852209ced335dd4b81ed49b8fc
KiddieD <djsndcx@gmail.com>
2020-12-05 11:02:43 +0100

    update volunteers day news

--- src/static/content/news-links.php
+++ src/static/content/news-links.php
@@ -1,4 +1,20 @@
-<div class="row">
+<div class="row"> 
+    <!-- Image -->
+    <div class="col-12 col-md-4 mb-1 text-center">
+        <a href="https://www.facebook.com/IrieVibesRootsFesitval/posts/1615410205327420">
+            <picture>
+                <source srcset="/src/img/news/volunteers-day.webp" type="image/webp">
+                <source srcset="/src/img/news/volunteers-day.jpg" type="image/jpeg">
+                <img alt="" class="img-fluid rounded w-80" src="/src/img/news/volunteers-day.jpg"/>
+            </picture>
+        </a>
+    </div>
+    <!-- Title & Text -->
+    <div class="col-12 col-md-8 mt-2">
+        <a href="https://www.facebook.com/IrieVibesRootsFesitval/posts/1615410205327420"><h4>Volunteers Day 2020</h4></a>
+        <small>Today is International Volunteer Day. A big shout out to all our volunteers : the ones who are there now, the ones who were there in the past and the one yet to come. Without you, there would never have been an Irie Vibes Roots Festival.<br/>
+            Let's hope we can meet again soon! Let's hope we can join energies and forces again in 2021!</small>
+    </div> 
     <!-- Image -->
     <div class="col-12 col-md-4 mb-1 text-center">
         <a href="https://shop.irievibes.be">
@@ -13,20 +29,5 @@
     <div class="col-12 col-md-8 mt-2">
         <a href="https://shop.irievibes.be"><h4>De Irie Vibes webshop</h4></a>
         <small>Je kan nu ook je favoriete items van het Irie Vibes Roots festival op onze webshop kopen. Koop je lidkaart vandaag, en geniet van 10% korting op de webshop en onze evenementen!</small>
-    </div> 
-    <!-- Image -->
-    <div class="col-12 col-md-4 mb-1 text-center">
-        <a href="https://mailchi.mp/e1c3fd6c8a65/nieuwsbrief-februari-4141742">
-            <picture>
-                <source srcset="/src/img/news/nieuwsbrief-februari-4141742.webp" type="image/webp">
-                <source srcset="/src/img/news/nieuwsbrief-februari-4141742.jpg" type="image/jpeg">
-                <img alt="" class="img-fluid rounded w-80" src="/src/img/news/nieuwsbrief-februari-4141742.jpg"/>
-            </picture>
-        </a>
     </div>
-    <!-- Title & Text -->
-    <div class="col-12 col-md-8 mt-2">
-        <a href="https://mailchi.mp/e1c3fd6c8a65/nieuwsbrief-februari-4141742"><h4>Nieuwsbrief Maart</h4></a>
-        <small>Meer info over Dub It Forward, Positivity Weekend foto's, ...</small>
-    </div>          
 </div>
