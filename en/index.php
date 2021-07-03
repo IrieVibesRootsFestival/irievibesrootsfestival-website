6c7619f4f38fbb71fa272808edc698bee6ce1ad8
HumbleDeer <djsndcx@gmail.com>
2021-07-03 12:12:12 +0200

    Small adjustments

--- en/index.php
+++ en/index.php
@@ -35,12 +35,12 @@
                         <hr />
                         <li><b>July 31st & August 1st × </b><a href="https://www.facebook.com/events/499653724640330/">West Vlaamse Vibes @ Zwanzibar</a> <small>[Summer Bar]</small></li>
                         <li><b>August 21st & 22nd × </b><a href="https://www.facebook.com/events/224091835982781/">Rock'n'Roll @ Zwanzibar</a> <small>[Summer Bar]</small></li>
-                        <li><b>September 11th & 12th × </b><a href="https://www.facebook.com/events/1199852113799184/">Woodstock @ Zwanzibar </a> <small>[Summer Bar]</small></li>
+                        <li><b>September 11th & 12th × </b><a href="https://www.facebook.com/events/1199852113799184/">Woodstock @ Zwanzibar</a> <small>[Summer Bar]</small></li>
                     </ul>
                     </p>
 
                 </div>
-                
+
                 <h1 class="mb-md-3 mt-2 mt-md-0 text-center quote">Irie Vibes Roots Festival</h1><br />
                 <div id="home_invite-text">
                     <div class="text-left text-success">
@@ -62,13 +62,13 @@
                     </div>
                 </div>
             </div>
-            <div class="col-12 col-md-4" id="lineup">
-            <div class="row p-2 m-0 mb-1 border border-lg border-warning rounded bg-img">
+            <div class="col-12 col-md-4" id="home_sidebar-right">
+                <div class="col p-2 m-0 mb-1 border border-lg border-success rounded bg-img">
                     <a href="https://tickets.irievibes.be" class="p-0">
                         <picture>
-                            <source srcset="src\img\promo\irie-vibes-2022-alt.webp" type="image/webp">
-                            <source srcset="src\img\promo\irie-vibes-2022-alt.jpg" type="image/jpeg">
-                            <img alt="" class="img-fluid rounded" src="src\img\promo\irie-vibes-2022-alt.jpg" />
+                            <source srcset="src/img/promo/irie-vibes-2022-alt.webp" type="image/webp">
+                            <source srcset="src/img/promo/irie-vibes-2022-alt.jpg" type="image/jpeg">
+                            <img alt="" class="img-fluid rounded" src="src/img/promo/irie-vibes-2022-alt.jpg" />
                         </picture>
                     </a>
                 </div>
