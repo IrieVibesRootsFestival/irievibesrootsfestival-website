e227d64de357b3d9a4ed336b6dba4d891d913d61
HumbleDeer <djsndcx@gmail.com>
2021-07-04 18:01:15 +0200

    test

--- src/static/pages/footer_en.php
+++ src/static/pages/footer_en.php
@@ -22,7 +22,11 @@
                         </div>
                     </div>
                 </form>
-                <!--End mc_embed_signup-->                
+                <!--End mc_embed_signup-->
+                <div class="input-group mb-3">
+                    <input type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="button-addon2">
+                    <button class="btn btn-outline-secondary" type="button" id="button-addon2">Button</button>
+                </div>
             </div>
         </div>
     </div>
