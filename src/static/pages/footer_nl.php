2e68fc6e4b5a19f29ef1cb4b876010f0ef62ceab
HumbleDeer <djsndcx@gmail.com>
2021-07-08 20:12:32 +0200

    fix footer thx

--- src/static/pages/footer_nl.php
+++ src/static/pages/footer_nl.php
@@ -1,78 +1,84 @@
 <!--Footer-->
-<footer id="footer_main">   
-    <div class="container pt-2">
-        <div class="row">
-            <div class="col-12 col-md-3 footer-brand animated fadeInLeft text-center">
-                <img src="/src/img/promo/greenforward.webp" class="img-fluid p-3 rounded-circle" alt="Greenforward Logo">
-            </div>
-            <div class="col-12 col-md-6 footer-nav animated fadeInUp">
-                <h4>— <i class="fas fa-info-circle"></i> Info —</h4>
-                <div class="row">
-                    <div class="col-6">
-                        <ul class="pages">
-                            <li><a href="/nl/info/faq/">FAQ</a></li>
-                            <li><a href="/nl/info/crew/">Crew</a></li>
-                            <li><a href="/nl/info/partners/">Partners</a></li>
-                            <li><a href="/src/static/content/newsletter/">Nieuwsbriefarchief</a></li>
-                        </ul>
-                    </div>
-                    <div class="col-6">
-                        <ul class="list">
-                            <li><a href="/nl/contact/">Contact</a></li>
-                            <li><a href="/nl/info/about/">Over Ons</a></li>
-                            <li><a href="/nl/info/trooper/"><img alt="" src="https://www.trooper.be/favicon.png" style="margin-top:-6px;"> Trooper</a></li>
-                        </ul>
+<footer id="footer_main">
+    <div id="footer_main">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 col-md-3 footer-brand animated fadeInLeft text-center">
+                    <img src="/src/img/promo/greenforward.webp" class="img-fluid p-3 rounded-circle" alt="Greenforward Logo">
+                </div>
+                <div id="footer_nav" class="col-12 col-md-6">
+                    <h4>— <i class="bi bi-info-circle-fill"></i> Info —</h4>
+                    <div class="row">
+                        <div class="col-6">
+                            <ul class="pages">
+                                <li><a href="/nl/info/faq/">FAQ</a></li>
+                                <li><a href="/nl/info/crew/">Crew</a></li>
+                                <li><a href="/nl/info/partners/">Partners</a></li>
+                                <li><a href="/src/static/content/newsletter/">Nieuwsbriefarchief</a></li>
+                            </ul>
+                        </div>
+                        <div class="col-6">
+                            <ul class="list">
+                                <li><a href="/nl/contact/">Contact</a></li>
+                                <li><a href="/nl/info/about/">Over Ons</a></li>
+                                <li><a href="/nl/info/trooper/"><img alt="" src="https://www.trooper.be/favicon.png" style="margin-top:-6px;"> Trooper</a></li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
+                <div class="col-12 col-md-3">
+                    <h4>— <i class="bi bi-newspaper"></i> Nieuwsbrief —</h4>
+                    <p class="py-1">Abonneer op onze nieuwsbrief</p>
+                    <span>
+                        <!-- Begin Mailchimp Signup Form -->
+                        <div id="mc_embed_signup">
+                            <form action="https://irievibes.us8.list-manage.com/subscribe/post?u=d99c4a0da87cf14cbb7acb09c&amp;id=ec0420bb34" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+                                <div id="mc_embed_signup_scroll">
+                                    <div class="form-group">
+                                        <label class="sr-only" for="subscribe-email">Emailadres</label>
+                                        <input type="text" value="" name="EMAIL" placeholder="Enter your email..." class="subscribe-email form-control required email" id="mce-EMAIL">
+                                    </div>
+                                    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+                                    <div style="position: absolute; left: -5000px;" aria-hidden="true">
+                                        <input type="text" name="b_81c9b73355e0e402d5618756c_8b879d422a" tabindex="-1" value="">
+                                    </div>
+                                    <div class="text-center">
+                                        <button type="submit" value="Subscribe" id="mc-embedded-subscribe" class="btn btn-volunteer mt-0"><a>Abonneer</a></button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                        <!--End mc_embed_signup-->
+                    </span>
+                </div>
             </div>
-            <div class="col-12 col-md-3">
-                <h4>— <i class="fas fa-newspaper"></i> Nieuwsbrief —</h4>
-                <p class="py-1">Abonneer op onze nieuwsbrief</p>
-                <span>
-                    <!-- Begin Mailchimp Signup Form -->
-                    <div id="mc_embed_signup">
-                        <form action="https://irievibes.us8.list-manage.com/subscribe/post?u=d99c4a0da87cf14cbb7acb09c&amp;id=ec0420bb34" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-                            <div id="mc_embed_signup_scroll">
-                                <div class="form-group w-100">
-                                    <label class="sr-only" for="subscribe-email">Email adres</label>
-                                    <input type="text" value="" name="EMAIL" placeholder="Enter your email..." class="subscribe-email form-control required email" id="mce-EMAIL">
-                                </div>
-                                <div id="mce-responses" class="clear">
-                                    <div class="response" id="mce-error-response" style="display:block"></div>
-                                    <div class="response" id="mce-success-response" style="display:block"></div>
-                                </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-                                <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_81c9b73355e0e402d5618756c_8b879d422a" tabindex="-1" value=""></div>
-                                <div class="text-center">
-                                    <button type="submit" value="Subscribe" id="mc-embedded-subscribe" class="btn btn-volunteer mt-0"><a>Abonneer</a></button>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
+            <div class="row">
 
-                    <!--End mc_embed_signup-->
-                </span>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-md-3 text-center order-1">
-                <a href="/en/info/volunteers" class="btn btn-volunteer">Word Vrijwilliger</a>
-            </div>
-            <div class="col-md-6 m-auto text-center d-none d-md-block order-3 order-md-2"><p>Copyright &copy; - vzw GreenForward 2001-2021</p></div>
-            <div class="col-md-3 order-2 order-md-3">
-                <div id="btn-social" class="text-center">
-                    <a href="https://www.facebook.com/IrieVibesRootsFesitval"><i class="fab fa-facebook fa-3x social"></i></a>
-                    <a href="https://www.instagram.com/irievibesroots/"><i class="fab fa-instagram fa-3x social"></i></a>
-                    <a href="/src/static/snapchat/"><i class="fab fa-snapchat fa-3x social"></i></a>
-                    <a href="/src/static/spotify/"><i class="fab fa-spotify fa-3x social"></i></a>
-                    <a href="https://www.youtube.com/channel/UCCWpbW7WkOyS0ycQdkqRhww"><i class="fab fa-youtube fa-3x social"></i></a>
+                <div class="col-md-6 m-auto text-center d-none d-md-block">
+                    <p>Copyright &copy; - vzw GreenForward 2001-2021</p>
+                </div>
+                <div class="col-md-3">
+                    <div id="footer_social-button" class="text-center">
+                        <a href="https://www.facebook.com/IrieVibesRootsFesitval"><i class="bi bi-facebook"></i></a>
+                        <a href="https://www.instagram.com/irievibesroots/"><i class="bi bi-instagram"></i></a>
+                        <a href="/src/static/snapchat/"><i class="bi bi-snapchat"></i></a>
+                        <a href="/src/static/spotify/"><i class="bi bi-spotify"></i></a>
+                        <a href="https://www.youtube.com/channel/UCCWpbW7WkOyS0ycQdkqRhww"><i class="bi bi-youtube"></i></a>
+                    </div>
+                </div>
+                <!-- Redundant for Mobile Compatibility-->
+                <div class="col-md-6 text-center d-block d-md-none">
+                    <p>Copyright &copy; - vzw GreenForward 2001-2021</p>
                 </div>
             </div>
         </div>
     </div>
+    <!-- START Bootstrap-Cookie-Alert -->
     <div class="alert text-center cookiealert" role="alert">
-        <p class="d-inline">Wij gebruiken cookies om zeker te weten dat je een goede ervaring hebt op onze website</p>
+        We gebruiken cookies om ervoor te zorgen dat jouw ervaring exact is hoe het hoort.
         <button type="button" class="btn btn-primary btn-sm acceptcookies" aria-label="Close">
             Oké
         </button>
     </div>
+    <!-- END Bootstrap-Cookie-Alert -->
 </footer>
