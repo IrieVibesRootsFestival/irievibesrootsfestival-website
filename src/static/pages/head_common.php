1cbee3d7f4d099c2c6b994026baae17925aa5bc2
HumbleDeer <djsndcx@gmail.com>
2021-07-10 15:03:22 +0200

    change og image

--- src/static/pages/head_common.php
+++ src/static/pages/head_common.php
@@ -14,7 +14,7 @@
 <meta name="Keywords" content="dub, reggae, ska, world music, food, festival, Irie, irie, Vibes, vibes, Roots, roots, Festival, festival, Koekelarestraat, 16, 8610, Kortemark, tickets, buy, presale, kids, belgium, music, 2021, back to the roots, lineup, artists, sunny after moon, fishermen, babbajah, dub up hifi, satta sound, salomon heritage, donovan kingjay, new kingston, uwe banton, jahmbassador, forward fever, earthworks">
 <meta name="google-site-verification" content="DvKM06H9VdWqi8wX1hms0vN2lBrqfGkRSIVzmvtCehM" /> 
 <!--Square OG Image (botched)-->
-<meta property="og:image" content="https://i.imgur.com/jNRMvrY.png" /> 
+<meta property="og:image" content="https://www.irievibes.be/src/img/promo/og_banner_2022_1.png" /> 
 <meta property="og:image:width" content="605" />
 <meta property="og:image:height" content="605" />
 <meta property="og:image:alt" content="Irie Vibes Roots Festival Buy Tickets" />  <!--Square OG Image (botched)-->
