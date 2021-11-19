e2e6e0d7fab4fb9464ef1165b1d1c4124fb21e4c
HumbleDeer <djsndcx@gmail.com>
2021-11-19 12:22:31 +0100

    try to fix js

--- src/static/pages/load_last.php
+++ src/static/pages/load_last.php
@@ -1,6 +1,6 @@
 
 <!-- Includes Popper + BootstrapJS -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 <!-- Cookie consent code -->
 <script src="https://cdn.jsdelivr.net/gh/Wruczek/Bootstrap-Cookie-Alert@gh-pages/cookiealert.js"></script>
 <script src="https://kit.fontawesome.com/62cb8cc953.js"></script>
