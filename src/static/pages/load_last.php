342bfef24fc143b92f1eafb45304229003404cf5
HumbleDeer <djsndcx@gmail.com>
2022-01-15 00:03:06 +0100

    oops, broke cookiealert, fixed it

--- src/static/pages/load_last.php
+++ src/static/pages/load_last.php
@@ -2,8 +2,8 @@
 <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script>
 <!-- Cookie consent code -->
-<script src="https://cdn.jsdelivr.net/gh/Wruczek/Bootstrap-Cookie-Alert@gh-pages/cookiealert.js"></script>
-<!-- <script src="https://kit.fontawesome.com/62cb8cc953.js"></script> -->
+<script src="/src/js/cookiealert.js"></script>
+
 <!-- Automatically add 'Active' class to Navbar -->
 <script>
     // get current URL path and assign 'active' class
