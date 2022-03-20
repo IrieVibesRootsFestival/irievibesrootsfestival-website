6e2ee381abf246d45d61ca22d87a9909f1df2d04
HumbleDeer <djsndcx@gmail.com>
2022-03-20 12:03:52 +0000

    Edit other headers

--- src/static/pages/header_en.php
+++ src/static/pages/header_en.php
@@ -10,7 +10,7 @@
             </div>
             <!--Main middle info-->
             <div class="col-12 col-lg-4">
-                <p class="text-warning text-uppercase m-0"><b> 22 - 23 - 24 July 2022 / Thursday free entry</b></p>
+                <p class="text-warning text-uppercase m-0"><b> 21 - 22 - 23 July 2022 / Thursday free!</b></p>
             </div>
             <!--Hashtag info-->
             <div class="col-12 col-lg-4 d-none d-lg-inline">
