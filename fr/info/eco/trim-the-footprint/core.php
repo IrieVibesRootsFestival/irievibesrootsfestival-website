15ed4941face2d3ceabdb242bde4c13e89c05808
Anna <djsndcx@gmail.com>
2022-04-01 20:57:04 +0200

    Add requested text to the Trim The Footprint pages

--- fr/info/eco/trim-the-footprint/core.php
+++ fr/info/eco/trim-the-footprint/core.php
@@ -1,67 +1,117 @@
 <div class="row">
-    <div class="col-12 col-md-12">                    
+    <div class="col-12 col-md-12">
         <h1 class="page-header">Trim The Footprint</h1>
-        <h3 class="text-irie text-uppercase "><b>Préface</b></h3>
+        <p class="muted"><small>Avec le soutien du Fonds européen de développement régional</small></p>
+        <div class="col-12 col-md-12">
+            <h3 class="text-irie text-uppercase "><b>Préface</b></h3>
+            <!--
         <picture>
-            <source srcset="/src/img/eco/trim-the-footprint/trimthefootprint.webp" type="image/webp">
             <source srcset="/src/img/eco/trim-the-footprint/trimthefootprint.png" type="image/png">
             <img alt="" class="img-fluid rounded float-end w-25" src="/src/img/eco/trim-the-footprint/trimthefootprint.png" alt="TTF Logo">
         </picture>
-        <p>
-            Le changement climatique nécessite certainement des solutions à un niveau international, national et individuel... Mais pour avoir un véritable impact, mondialement, il faut surtout changer de modèle économique et social. Pour y arriver, il faut collaborer au delà de nos frontières nationales. 
-        </p>
-        <p>
-            <a href="https://www.laconditionpublique.com/">La Condition Publique <small>(Roubaix)</small></a>, le festival <a href="https://www.esperanzah.be/">Esperanzah! <small>(Floreffe)</small></a> et le Irie Vibes Roots Festival <small>(Kortemark)</small>, tous les trois actifs dans le secteur socio-culturel, unissent leurs forces pour non seulement renforcer leurs expériences d'évènements durables, mais aussi pour dynamiser le changement auprès des autorités à travers la sensibilisation de leurs partenaires et visiteurs. A notre échelle <small>#OnFaitCeQueOnPeut</small>
-        </p>
-        <p>
-            Ce projet est soutenu par <a href="https://www.interreg-fwvl.eu/nl">Interreg France-Wallonie-Vlaanderen</a> et porte sur l'écologie, l'éducation et les langues. <small>#Trimthefootprint</small>
-        </p>
-    </div>
-    <div class="col-12">
-        <h3 class="text-irie text-uppercase"><b>Les quatre axes du projet:</b></h3>
-        <div class="text-box-2 rounded p-3">
-            <div class="text-box rounded p-3">
-                <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
-                    <li class="nav-item">
-                        <a class="nav-link active" id="tab-1" data-toggle="pill" href="#conscious-communication" role="tab" aria-controls="conscious-communication" aria-selected="true">Conscious communication</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" id="tab-2" data-toggle="pill" href="#trim-the-footprint" role="tab" aria-controls="trim-the-footprint" aria-selected="false">Trim The Footprint</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" id="tab-3" data-toggle="pill" href="#changing-minds" role="tab" aria-controls="changing-minds" aria-selected="false">Changing minds</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" id="tab-4" data-toggle="pill" href="#food-waste-facts" role="tab" aria-controls="food-waste-facts" aria-selected="false">Food waste facts</a>
-                    </li>
-                </ul>
-                <div class="tab-content rounded p-0 p-md-2" id="TrimTheFootprint-tab">
-                    <div class="tab-pane fade show active" id="conscious-communication" role="tabpanel" aria-labelledby="tab-1">
-                        <h4 class="text-irie"><b>Axis 1</b></h4>
-                        <p>
-                            Par une communication systématique sur les sites internet, via des newsletters et par les médias sociaux, nous souhaitons élargir la portée du projet, et le faire grandir au-delà du groupe cible de chaque action particulière. A travers les médias sociaux, nous encouragerons l'interactivité avec le public. Le projet sera visuellement reconnaissable grâce à l'usage d'un slogan et d'un logo communs. En invitant des organisateurs d'autres évènements, nous espérons que certaines actions se propagerons au-delà duprésent projet. En incluant le monde de l'enseignement, nous voulons engendrer des résultats à long terme.
-                        </p>
-                    </div>
-                    <div class="tab-pane fade" id="trim-the-footprint" role="tabpanel" aria-labelledby="tab-2">
-                        <h4 class="text-irie"><b>Axis 2</b></h4>
-                        <p>
-                            Nous souhaitons calculer l'empreinte carbone de chaque partenaire sur la base des informations disponibles pour l'événement 2019 et suivant l'échelle de prestations de CO2. Ensuite nous voulons pour chaque partenaire, mais aussi trans-événementiel, discuter en concertation des possibles mesures concernant la consommation énergétique, les déchets, la mobilité, l'alimentation et l'eau. A ce sujet, nous tenterons de tenir compte du prix d'achat, de la communication, de la fiabilité et de la durabilité. De cette manière, nous réduirons non seulement notre propre empreinte, mais nous serons également un exemple pour d'autres évènements.
-                        </p>
-                    </div>
-                    <div class="tab-pane fade" id="changing-minds" role="tabpanel" aria-labelledby="tab-3">
-                        <h4 class="text-irie"><b>Axis 3</b></h4>
-                        <p>
-                            Les festivaliers seront abordés d'une manière stimulante : la musique les attire, mais de l'espace est créé pour découvrir aussi quelque chose d'autre. Ainsi, nous espérons encourager les gens à adopter des changements concrets dans leur vie quotidienne. En offrant à des organisations écologiques l'espace et la possibilité de présenter leur travail à un large publique, nous les stimulons aussi à perpétuer le développement de leur travail. En invitant d'autres organisateurs d'évènements et les autorités locales, nous espérons diffuser "un feu écologique' qui continuera de brûler au-delà du projet. Nous réduisons les obstacles et rassemblons les autorités, des organisations évènementielles et écologiques, et le public de festival pour créer des effets à long terme.
-                        </p>
-                    </div>
-                    <div class="tab-pane fade" id="food-waste-facts" role="tabpanel" aria-labelledby="tab-4">
-                        <h4 class="text-irie"><b>Axis 4</b></h4>
-                        <p>
-                            Nous voulons créer ensemble un recueil bilingue pour les 10-12 ans qui comprend des explications sur le gaspillage alimentaire. Ce recueil mettra en avant cinq recettes (cuisiner avec des restes, cuisiner avec des déchets). A cette fin, nous prendrons contact avec divers chefs de cuisine. Le recueil sera disponible tant digitalement qu'en version papier. Nous tiendrons compte de méthodes créatives et inspirantes dans l'élaboration du recueil. Avec ce recueil, nous souhaitons nous adresser aux diverses écoles. Il sera proposé gratuitement. Le cas échéant, un membre des équipes des partenaires pourra présenter le recueil par le biais d'un atelier dans les écoles. En établissant un pont avec l'enseignement, nous souhaitons lier nos événements à la vie locale et nous voulons ancrer le projet dans l'avenir.
-                        </p>
+-->
+            <p>
+                Le changement climatique nécessite certainement des solutions à un niveau international, national et
+                individuel... Mais pour avoir un véritable impact, mondialement, il faut surtout changer de modèle
+                économique et social. Pour y arriver, il faut collaborer au delà de nos frontières nationales.
+            </p>
+            <p>
+                <a href="https://www.laconditionpublique.com/">La Condition Publique <small>(Roubaix)</small></a>, le
+                festival <a href="https://www.esperanzah.be/">Esperanzah! <small>(Floreffe)</small></a> et le Irie Vibes
+                Roots Festival <small>(Kortemark)</small>, tous les trois actifs dans le secteur socio-culturel,
+                unissent leurs forces pour non seulement renforcer leurs expériences d'évènements durables, mais aussi
+                pour dynamiser le changement auprès des autorités à travers la sensibilisation de leurs partenaires et
+                visiteurs. A notre échelle <small>#OnFaitCeQueOnPeut</small>
+            </p>
+            <p>
+                Ce projet est soutenu par <a href="https://www.interreg-fwvl.eu/nl">Interreg
+                    France-Wallonie-Vlaanderen</a> et porte sur l'écologie, l'éducation et les langues.
+                <small>#Trimthefootprint</small>
+            </p>
+        </div>
+        <div class="col-12">
+            <h3 class="text-irie text-uppercase"><b>Les quatre axes du projet:</b></h3>
+            <div class="text-box-2 rounded p-3">
+                <div class="text-box rounded p-3">
+                    <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
+                        <li class="nav-item">
+                            <a class="nav-link active" id="tab-1" data-toggle="pill" href="#conscious-communication"
+                                role="tab" aria-controls="conscious-communication" aria-selected="true">Conscious
+                                communication</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" id="tab-2" data-toggle="pill" href="#trim-the-footprint" role="tab"
+                                aria-controls="trim-the-footprint" aria-selected="false">Trim The Footprint</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" id="tab-3" data-toggle="pill" href="#changing-minds" role="tab"
+                                aria-controls="changing-minds" aria-selected="false">Changing minds</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" id="tab-4" data-toggle="pill" href="#food-waste-facts" role="tab"
+                                aria-controls="food-waste-facts" aria-selected="false">Food waste facts</a>
+                        </li>
+                    </ul>
+                    <div class="tab-content rounded p-0 p-md-2" id="TrimTheFootprint-tab">
+                        <div class="tab-pane fade show active" id="conscious-communication" role="tabpanel"
+                            aria-labelledby="tab-1">
+                            <h4 class="text-irie"><b>Axis 1</b></h4>
+                            <p>
+                                Par une communication systématique sur les sites internet, via des newsletters et par
+                                les médias sociaux, nous souhaitons élargir la portée du projet, et le faire grandir
+                                au-delà du groupe cible de chaque action particulière. A travers les médias sociaux,
+                                nous encouragerons l'interactivité avec le public. Le projet sera visuellement
+                                reconnaissable grâce à l'usage d'un slogan et d'un logo communs. En invitant des
+                                organisateurs d'autres évènements, nous espérons que certaines actions se propagerons
+                                au-delà duprésent projet. En incluant le monde de l'enseignement, nous voulons engendrer
+                                des résultats à long terme.
+                            </p>
+                        </div>
+                        <div class="tab-pane fade" id="trim-the-footprint" role="tabpanel" aria-labelledby="tab-2">
+                            <h4 class="text-irie"><b>Axis 2</b></h4>
+                            <p>
+                                Nous souhaitons calculer l'empreinte carbone de chaque partenaire sur la base des
+                                informations disponibles pour l'événement 2019 et suivant l'échelle de prestations de
+                                CO2. Ensuite nous voulons pour chaque partenaire, mais aussi trans-événementiel,
+                                discuter en concertation des possibles mesures concernant la consommation énergétique,
+                                les déchets, la mobilité, l'alimentation et l'eau. A ce sujet, nous tenterons de tenir
+                                compte du prix d'achat, de la communication, de la fiabilité et de la durabilité. De
+                                cette manière, nous réduirons non seulement notre propre empreinte, mais nous serons
+                                également un exemple pour d'autres évènements.
+                            </p>
+                        </div>
+                        <div class="tab-pane fade" id="changing-minds" role="tabpanel" aria-labelledby="tab-3">
+                            <h4 class="text-irie"><b>Axis 3</b></h4>
+                            <p>
+                                Les festivaliers seront abordés d'une manière stimulante : la musique les attire, mais
+                                de l'espace est créé pour découvrir aussi quelque chose d'autre. Ainsi, nous espérons
+                                encourager les gens à adopter des changements concrets dans leur vie quotidienne. En
+                                offrant à des organisations écologiques l'espace et la possibilité de présenter leur
+                                travail à un large publique, nous les stimulons aussi à perpétuer le développement de
+                                leur travail. En invitant d'autres organisateurs d'évènements et les autorités locales,
+                                nous espérons diffuser "un feu écologique' qui continuera de brûler au-delà du projet.
+                                Nous réduisons les obstacles et rassemblons les autorités, des organisations
+                                évènementielles et écologiques, et le public de festival pour créer des effets à long
+                                terme.
+                            </p>
+                        </div>
+                        <div class="tab-pane fade" id="food-waste-facts" role="tabpanel" aria-labelledby="tab-4">
+                            <h4 class="text-irie"><b>Axis 4</b></h4>
+                            <p>
+                                Nous voulons créer ensemble un recueil bilingue pour les 10-12 ans qui comprend des
+                                explications sur le gaspillage alimentaire. Ce recueil mettra en avant cinq recettes
+                                (cuisiner avec des restes, cuisiner avec des déchets). A cette fin, nous prendrons
+                                contact avec divers chefs de cuisine. Le recueil sera disponible tant digitalement qu'en
+                                version papier. Nous tiendrons compte de méthodes créatives et inspirantes dans
+                                l'élaboration du recueil. Avec ce recueil, nous souhaitons nous adresser aux diverses
+                                écoles. Il sera proposé gratuitement. Le cas échéant, un membre des équipes des
+                                partenaires pourra présenter le recueil par le biais d'un atelier dans les écoles. En
+                                établissant un pont avec l'enseignement, nous souhaitons lier nos événements à la vie
+                                locale et nous voulons ancrer le projet dans l'avenir.
+                            </p>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
