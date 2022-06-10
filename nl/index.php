62b28dd1d9934af7a97a02a9fb26104f54bc65e8
Anna <djsndcx@gmail.com>
2022-06-10 21:17:57 +0200

    Modify the front pages

--- nl/index.php
+++ nl/index.php
@@ -1,66 +1,25 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <!-- Import meta tags, OpenGraphs, stylesheets and common includes -->
     <?php include($_SERVER['DOCUMENT_ROOT'] . '/src/page_parts/head_common.php'); ?>
     <title>Home - Irie Vibes</title>
     <meta property="og:title" content="Home" />
-    <meta property="og:description" content="Back in time to where it all began, Irie Vibes invites you to their Back to The Roots edition during the heart of summer 2022 in Kortemark." />
+    <meta property="og:description"
+        content="Back in time to where it all began, Irie Vibes invites you to their Back to The Roots edition during the heart of summer 2022 in Kortemark." />
 </head>
 
 <body>
     <?php include($_SERVER['DOCUMENT_ROOT'] . '/nl/_page_parts/header_nl.php'); ?>
     <div class="container content">
         <div class="row justify-content-center">
-        <div class="col-12 col-md-8" id="home_sidebar-left">
-        <picture>
+            <div class="col-12 col-md-8" id="home_sidebar-left">
+                <picture>
                     <source srcset="/src/img/promo/irievibes-banner-2022.webp" type="image/webp">
                     <source srcset="/src/img/promo/irievibes-banner-2022.jpg" type="image/jpeg">
                     <img alt="" class="img-fluid rounded" src="/src/img/promo/irievibes-banner-2022.jpg" />
                 </picture>
-        </div>
-            <div class="col-12 col-md-4" id="home_sidebar-right">
-                <div class="box border border-irie-green">
-                    <a href="https://shop.irievibes.be" class="p-0">
-                        <picture>
-                            <source srcset="/src/img/news/irie-vibes-trui-webshop-advertentie-small.webp" type="image/webp">
-                            <source srcset="/src/img/news/irie-vibes-trui-webshop-advertentie-small.jpg" type="image/jpeg">
-                            <img alt="" class="img-fluid" src="/src/img/news/irie-vibes-trui-webshop-advertentie-small.jpg" />
-                        </picture>
-                    </a>
-                </div>
-            </div>
-            <div id="home-text" class="col-12 col-md-8 ">
-                <h1 class="mb-md-3 mt-2 mt-md-0 text-center quote">Irie Vibes Roots Festival</h1><br />
-                <div id="home_invite-text">
-                    <div id="home_invite-first" class="text-end text-irie-green pe-md-4">
-                        <h1 class="mt-md-3 mb-0 px-3 quote">back to the roots</h1>
-                        <b>
-                            <p class="text-justify-right">
-                            <p class="text-justify">
-                                Irie Vibes gaat terug naar waar het allemaal begon. Irie Vibes 2022 zal opnieuw een terugblik zijn naar onze roots, herinneringen en gebeurend. We nodigen je uit om deel uit te maken van het <b>Irie Vibes back to the Roots Festival 2022</b>, waar hoofd, hart en lichaam samen komen in een fantastisch spektakel van kleur en geluid. Mis geen enkele drumslag! Plaats een bookmark van onze
-                                <a class="text-irie-green" href="/en/lineup/"><i class="bi bi-link-45deg"></i><b>line-up pagina</b></a> op je startscherm. Irie!
-                            </p>
-                            </p>
-                            <p>
-                                Nog steeds een drang voor meer? Plaats een bookmark op onze paginas! Je kan meer info vinden in de footer, of <a class="text-irie-success" href="#footer_main"><i class="bi bi-info-circle"></i><b>hier</b></a> klikken.
-                            </p>
-                        </b>
-                    </div>
-                    <div id="home_invite-second" class="text-left text-irie-yellow">
-                        <h1 class="mt-3 mb-0 px-3 quote">one love</h1>
-
-
-                        <p class="text-justify">
-                            Hier bij Irie Vibes is familie belangrijk. Als een grote familie die bouwt naar een nóg grotere toekomst. Zonder hulp van mensen zoals jij zou het onmogelijk zijn om deze droom waar te maken. Mash down! Ga naar de <a class="text-warning" href="/nl/info/volunteers/"><i class="fas fa-link fa-xs" style="font-size: 0.6rem;"></i>vrijwilligers pagina</a> om te kijken als het iets voor jou is. Family thing!
-                    </div>
-                    <div id="home_invite-third" class="text-end text-irie-red pe-md-4">
-                        <h1 class="mt-3 mb-0 px-3 quote ">Wees erbij</h1>
-                        <p class="text-justify">
-                            Op zoek naar tickets? Koop ze nu! Een camping ticket garandeert dat je een plaatsje op de camping krijgt. Tickets en prijzen kan je <a class="text-danger" href="/nl/tickets/"><i class="fas fa-link fa-xs" style="font-size: 0.6rem;"></i>hier</a> vinden. Vroege vogels kunnen zich wagen met een Early Bird ticket, en een korting scoren!
-                        </p>
-                    </div>
-                </div>
             </div>
             <div class="col-12 col-md-4" id="home_sidebar-right">
                 <div class="box border border-dark">
@@ -73,7 +32,7 @@
                     </a>
                 </div>
                 <div class="box border border-irie-red">
-                    <a href="">
+                    <a href="./festival/kortemark-conge/">
                         <picture>
                             <source srcset="" type="image/webp">
                             <source srcset="/src/img/kortemark-conge/kortemark_conge.jpg" type="image/jpeg">
@@ -90,16 +49,105 @@
                         </picture>
                     </a>
                 </div>
-                <div class="box border border-wood">
+                <div class="box border border-irie-green">
                     <a href="https://shop.irievibes.be" class="p-0">
                         <picture>
-                            <source srcset="/src/img/news/irie-vibes-trui-webshop-advertentie-small.webp" type="image/webp">
-                            <source srcset="/src/img/news/irie-vibes-trui-webshop-advertentie-small.jpg" type="image/jpeg">
-                            <img alt="" class="img-fluid" src="/src/img/news/irie-vibes-trui-webshop-advertentie-small.jpg" />
+                            <source srcset="/src/img/news/irie-vibes-trui-webshop-advertentie-small.webp"
+                                type="image/webp">
+                            <source srcset="/src/img/news/irie-vibes-trui-webshop-advertentie-small.jpg"
+                                type="image/jpeg">
+                            <img alt="" class="img-fluid"
+                                src="/src/img/news/irie-vibes-trui-webshop-advertentie-small.jpg" />
                         </picture>
                     </a>
                 </div>
-                <!--
+            </div>
+        </div>
+        <div id="home-text" class="col-12 col-md-8 ">
+            <h1 class="mb-md-3 mt-2 mt-md-0 text-center quote">Irie Vibes Roots Festival</h1><br />
+            <div id="home_invite-text">
+                <div id="home_invite-first" class="text-end text-irie-green pe-md-4">
+                    <h1 class="mt-md-3 mb-0 px-3 quote">back to the roots</h1>
+                    <b>
+                        <p class="text-justify-right">
+                        <p class="text-justify">
+                            Irie Vibes gaat terug naar waar het allemaal begon. Irie Vibes 2022 zal opnieuw een
+                            terugblik zijn naar onze roots, herinneringen en gebeurend. We nodigen je uit om deel uit te
+                            maken van het <b>Irie Vibes back to the Roots Festival 2022</b>, waar hoofd, hart en lichaam
+                            samen komen in een fantastisch spektakel van kleur en geluid. Mis geen enkele drumslag!
+                            Plaats een bookmark van onze
+                            <a class="text-irie-green" href="/en/lineup/"><i class="bi bi-link-45deg"></i><b>line-up
+                                    pagina</b></a> op je startscherm. Irie!
+                        </p>
+                        </p>
+                        <p>
+                            Nog steeds een drang voor meer? Plaats een bookmark op onze paginas! Je kan meer info vinden
+                            in de footer, of <a class="text-irie-success" href="#footer_main"><i
+                                    class="bi bi-info-circle"></i><b>hier</b></a> klikken.
+                        </p>
+                    </b>
+                </div>
+                <div id="home_invite-second" class="text-left text-irie-yellow">
+                    <h1 class="mt-3 mb-0 px-3 quote">one love</h1>
+
+
+                    <p class="text-justify">
+                        Hier bij Irie Vibes is familie belangrijk. Als een grote familie die bouwt naar een nóg grotere
+                        toekomst. Zonder hulp van mensen zoals jij zou het onmogelijk zijn om deze droom waar te maken.
+                        Mash down! Ga naar de <a class="text-warning" href="/nl/info/volunteers/"><i
+                                class="fas fa-link fa-xs" style="font-size: 0.6rem;"></i>vrijwilligers pagina</a> om te
+                        kijken als het iets voor jou is. Family thing!
+                </div>
+                <div id="home_invite-third" class="text-end text-irie-red pe-md-4">
+                    <h1 class="mt-3 mb-0 px-3 quote ">Wees erbij</h1>
+                    <p class="text-justify">
+                        Op zoek naar tickets? Koop ze nu! Een camping ticket garandeert dat je een plaatsje op de
+                        camping krijgt. Tickets en prijzen kan je <a class="text-danger" href="/nl/tickets/"><i
+                                class="fas fa-link fa-xs" style="font-size: 0.6rem;"></i>hier</a> vinden. Vroege vogels
+                        kunnen zich wagen met een Early Bird ticket, en een korting scoren!
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="col-12 col-md-4" id="home_sidebar-right">
+            <div class="box border border-dark">
+                <a href="https://tickets.irievibes.be">
+                    <picture>
+                        <source srcset="/src/img/promo/irie-vibes-2022-alt.webp" type="image/webp">
+                        <source srcset="/src/img/promo/irie-vibes-2022-alt.jpg" type="image/jpeg">
+                        <img alt="" class="img-fluid" src="/src/img/promo/irie-vibes-2022-alt.jpg" />
+                    </picture>
+                </a>
+            </div>
+            <div class="box border border-irie-red">
+                <a href="">
+                    <picture>
+                        <source srcset="" type="image/webp">
+                        <source srcset="/src/img/kortemark-conge/kortemark_conge.jpg" type="image/jpeg">
+                        <img alt="" class="img-fluid" src="/src/img/kortemark-conge/kortemark_conge.jpg" />
+                    </picture>
+                </a>
+            </div>
+            <div class="box border border-irie-yellow">
+                <a href="./kids/">
+                    <picture>
+                        <source srcset="" type="image/webp">
+                        <source srcset="\src\img\little-lion-banner.png" type="image/jpeg">
+                        <img alt="" class="img-fluid" src="\src\img\little-lion-banner.png" />
+                    </picture>
+                </a>
+            </div>
+            <div class="box border border-wood">
+                <a href="https://shop.irievibes.be" class="p-0">
+                    <picture>
+                        <source srcset="/src/img/news/irie-vibes-trui-webshop-advertentie-small.webp" type="image/webp">
+                        <source srcset="/src/img/news/irie-vibes-trui-webshop-advertentie-small.jpg" type="image/jpeg">
+                        <img alt="" class="img-fluid"
+                            src="/src/img/news/irie-vibes-trui-webshop-advertentie-small.jpg" />
+                    </picture>
+                </a>
+            </div>
+            <!--
                 <div class="row py-2 m-0 border border-lg border-success rounded bg-img">
                     <div class="col-12">
                         <p class="text-center bg-success rounded text-white">Lion's Den</p>
@@ -114,46 +162,48 @@
 
                     </div>
                 </div>-->
-                <h1 class="mb-md-3 mt-2 mt-md-0 text-center quote">Zie dit!</h1>
-                <div id="slider-wrapper" class="p-2 mb-2 rounded">
-                    <?php include($_SERVER['DOCUMENT_ROOT'] . '/src/static/content/slider.php'); ?>
-                </div>
-                <h1 class="mb-md-3 mt-2 mt-md-0 text-center quote">Aftermovie</h1>
-                <div id="slider-wrapper" class="rounded p-2 mb-1">
-                    <style>
-                        .embed-container {
-                            position: relative;
-                            padding-bottom: 43%;
-                            height: 0;
-                            overflow: hidden;
-                            max-width: 100%;
-                        }
-
-                        .embed-container iframe,
-                        .embed-container object,
-                        .embed-container embed {
-                            position: absolute;
-                            top: 0;
-                            left: 0;
-                            width: 100%;
-                            height: 100%;
-                        }
-                    </style>
-                    <div class='embed-container'><iframe src='https://player.vimeo.com/video/371957649?autoplay=1&portrait=false&muted=true' frameborder='0' webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe></div>
-                </div>
+            <h1 class="mb-md-3 mt-2 mt-md-0 text-center quote">Zie dit!</h1>
+            <div id="slider-wrapper" class="p-2 mb-2 rounded">
+                <?php include($_SERVER['DOCUMENT_ROOT'] . '/src/static/content/slider.php'); ?>
             </div>
-            <div class="col-12">
-                <h1 class="mb-md-3 mt-2 mt-md-0 text-center quote">music unites</h1><br />
-            </div>
+            <h1 class="mb-md-3 mt-2 mt-md-0 text-center quote">Aftermovie</h1>
+            <div id="slider-wrapper" class="rounded p-2 mb-1">
+                <style>
+                .embed-container {
+                    position: relative;
+                    padding-bottom: 43%;
+                    height: 0;
+                    overflow: hidden;
+                    max-width: 100%;
+                }
 
-            <div class="col-12 col-md-4">
-                <div class="col">
-                    <h3 class="text-irie"><b>Nieuws & meer</b></h3>
-                    <!--Inserted elements here -->
-                    <?php include($_SERVER['DOCUMENT_ROOT'] . '/src/static/content/news-links.php'); ?>
-                </div>
+                .embed-container iframe,
+                .embed-container object,
+                .embed-container embed {
+                    position: absolute;
+                    top: 0;
+                    left: 0;
+                    width: 100%;
+                    height: 100%;
+                }
+                </style>
+                <div class='embed-container'><iframe
+                        src='https://player.vimeo.com/video/371957649?autoplay=1&portrait=false&muted=true'
+                        frameborder='0' webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe></div>
             </div>
         </div>
+        <div class="col-12">
+            <h1 class="mb-md-3 mt-2 mt-md-0 text-center quote">music unites</h1><br />
+        </div>
+
+        <div class="col-12 col-md-4">
+            <div class="col">
+                <h3 class="text-irie"><b>Nieuws & meer</b></h3>
+                <!--Inserted elements here -->
+                <?php include($_SERVER['DOCUMENT_ROOT'] . '/src/static/content/news-links.php'); ?>
+            </div>
+        </div>
+    </div>
     </div>
     <!-- Include Footer -->
     <?php include($_SERVER['DOCUMENT_ROOT'] . '/src/static/pages/footer_nl.php'); ?>
@@ -161,6 +211,7 @@
 <!--Loading bootstrap and jQuery scripts at the end of the page to prevent slowdowns-->
 <?php include($_SERVER['DOCUMENT_ROOT'] . '/src/static/pages/load_last.php'); ?>
 <div id="fb-root"></div>
-<script async defer crossorigin="anonymous" src="https://connect.facebook.net/nl_BE/sdk.js#xfbml=1&version=v6.0"></script>
+<script async defer crossorigin="anonymous" src="https://connect.facebook.net/nl_BE/sdk.js#xfbml=1&version=v6.0">
+</script>
 
 </html>
