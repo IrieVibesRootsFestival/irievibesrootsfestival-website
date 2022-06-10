03f0fc39f1de03eb8c8c78ee3c2294045ead295d
Anna <djsndcx@gmail.com>
2022-06-10 22:47:46 +0200

    rm Coop, Partners "static", change to foreach itt
    Possible fix for PHP script
    
    did an oopsie
    
    Print php info 32/path translated (debugging)
    
    Possible fix for faulty loop root address
    
    debugging the partners script
    
    Fix php routine

--- en/info/partners/index.php
+++ en/info/partners/index.php
@@ -1,25 +1,39 @@
-<!DOCTYPE html><html lang="en">
-    <head>
-        <!-- Import meta tags, OpenGraphs, stylesheets and common includes -->
-        <?php include($_SERVER['DOCUMENT_ROOT'].'/src/page_parts/head_common.php');?>
-        <title>Partners - Irie Vibes</title>
-        <meta property="og:title" content="Partners & Cooperations"/>
-        <meta property="og:description" content="View our partners and cooperations. Click an image to discover." />
-    </head> 
-    <body>
-        <?php include($_SERVER['DOCUMENT_ROOT'].'/en/_page_parts/header_en.php');?>
-        <!-- This is the main container that encapsulates everything content related. -->
-        <div class="container content">
-            <br/>
-            <h1 class="page-header">Partners</h1>
-            <div class="row"><?php include($_SERVER['DOCUMENT_ROOT'].'/src/static/content/partners.php');?></div>
-            <h1 class="page-header">Cooperations</h1>
-            <div class="row"><?php include($_SERVER['DOCUMENT_ROOT'].'/src/static/content/cooperations.php');?></div>
-        <p class="mt-2 mb-2 text-center"><span class="text-danger">The sponsor list is <u>not</u> complete!</span> Estimated date of completion is April.</p>
-        </div>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Import meta tags, OpenGraphs, stylesheets and common includes -->
+    <?php include($_SERVER['DOCUMENT_ROOT'] . '/src/page_parts/head_common.php'); ?>
+    <title>Partners - Irie Vibes</title>
+    <meta property="og:title" content="Partners & Cooperations" />
+    <meta property="og:description" content="View our partners and cooperations." />
+</head>
+
+<body>
+    <?php include($_SERVER['DOCUMENT_ROOT'] . '/en/_page_parts/header_en.php'); ?>
+    <!-- This is the main container that encapsulates everything content related. -->
+    <div class="container content">
+        <p>test</p>
+        <?php
+        $dir_name = $_SERVER['DOCUMENT_ROOT'] . '/src/img/partners/';
+        $images = glob($dir_name . "*.jpg");
+        foreach ($images as $image) {
+            $image_url = 'https://' . $_SERVER['HTTP_HOST'] . '/src/img/partners/' . basename($image, '.jpg');
+            echo ('
+            <div class="col-3 col-sm-3 my-2">
+                <picture>
+                    <source srcset="' . $image_url . '.webp" type="image/webp">
+                    <img alt="' . basename($image, '.jpg') . '" class="img-fluid rounded" src="' . $image_url . '.jpg" type="image/jpeg">
+                </picture>
+            </div>
+            ');
+        }
+        ?>
+    </div>
     <!-- Include Footer -->
-    <?php include($_SERVER['DOCUMENT_ROOT']."/en/page_parts/footer_en.php");?>        
-    </body>
+    <?php include($_SERVER['DOCUMENT_ROOT'] . "/en/page_parts/footer_en.php"); ?>
+</body>
 <!--Loading bootstrap and jQuery scripts at the end of the page to prevent slowdowns-->
-<?php include($_SERVER['DOCUMENT_ROOT'].'/src/static/pages/load_last.php');?>
+<?php include($_SERVER['DOCUMENT_ROOT'] . '/src/static/pages/load_last.php'); ?>
+
 </html>
