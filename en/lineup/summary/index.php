e8a35af1ad776eeb287cf650a35648ffe8d9ea76
Anna <djsndcx@gmail.com>
2022-06-11 01:52:22 +0200

    LineUp Tables (en)

---
+++ en/lineup/summary/index.php
@@ -0,0 +1,182 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Import meta tags, OpenGraphs, stylesheets and common includes -->
+    <?php include($_SERVER['DOCUMENT_ROOT'] . '/_page_parts/head_common.php'); ?>
+    <title>Line-Up - Irie Vibes</title>
+    <meta property="og:title" content="Partenaires" />
+    <meta property="og:description" content="Voir nos partenaires de cette année" />
+</head>
+
+<body>
+    <?php include($_SERVER['DOCUMENT_ROOT'] . "/en/_page_parts/header_en.php"); ?>
+    <div class="container content">
+        <div class="row justify-content-center">
+            <div class="col-12 text-center">
+                <h1 class="page-header">Line-Up</h1>
+            </div>
+            <div class="col-12">
+                <table class="table table-striped table-bordered table-responsive table-sm">
+                    <thead>
+                        <tr>
+                            <th scope="row" colspan="5">Thursday</th>
+                        </tr>
+                        <tr>
+                            <th scope="col"><i class="fa-regular fa-clock"></i></th>
+                            <th scope="col">Lion's Den</th>
+                            <th scope="col">Inner Circle</th>
+                            <th scope="col">Little Lion</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row">18:30</td>
+                            <td></td>
+                            <td>Zhendria <br>“interactief boekvoorstel Actieve Hoop”</td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <th scope="row">19:00</td>
+                            <td>Ten Square Feet</td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <th scope="row">20:00</td>
+                            <td></td>
+                            <td>Honingbijen als sleutelfiguur in de ecologische kringloop?</td>
+                            <td>Zhendria<br>Qi Gong met de kids</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">20:30</td>
+                            <td>Martioanen</td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <th scope="row">21:30</td>
+                            <td>Bangarang</td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <th scope="row">22:30</td>
+                            <td>Fheeta &amp; The Westfrontband</td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <th scope="row">23:30</td>
+                            <td>Bounty Radio</td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div class="col-12">
+                <table class="table table-striped table-bordered table-responsive table-sm">
+                    <thead>
+                        <tr>
+                            <th scope="row" colspan="4">Friday</th>
+                        </tr>
+                        <tr>
+                            <th scope="col"><i class="fa-regular fa-clock"></i></td>
+                            <th scope="col">Lion's Den</th>
+                            <th scope="col">Inner Circle</th>
+                            <th scope="col">Camping</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row">12:00</td>
+                            <td></td>
+                            <td></td>
+                            <td>Satta Sound</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">18:00</td>
+                            <td>Youth &amp; Truth<br>Agobun<br>Jah Militant</td>
+                            <td>Jazzavan</td>
+                            <td></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div class="col-12">
+                <table class="table table-striped table-bordered table-responsive table-sm">
+                    <thead>
+                        <tr>
+                            <th scope="row" colspan="4">Saturday</th>
+                        </tr>
+                        <tr>
+                            <th scope="col"><i class="fa-regular fa-clock"></i></th>
+                            <th scope="col">Lion's Den</th>
+                            <th scope="col">Inner Circle</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row">12:00</td>
+                            <td></td>
+                            <td>Jahwa Roots Selection</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">14:00</td>
+                            <td>Eazzup</td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <th scope="row">15:00</td>
+                            <td></td>
+                            <td>Terra Soundsystem</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">16:30</td>
+                            <td>Spellbreakers</td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <th scope="row">17:30</td>
+                            <td></td>
+                            <td>Shikoba Soundsystem</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">19:00</td>
+                            <td>Rapha Pico<br>Shiwa (Noble Chanters)</td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <th scope="row">20:30</td>
+                            <td></td>
+                            <td>Manasseh</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">22:30</td>
+                            <td>Micah Shemaiah<br>&amp; The Dreadites</td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <th scope="row">24:00</td>
+                            <td>Vivian Jones</td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <th scope="row">2:00</td>
+                            <td>Dub-Up HiFi</td>
+                            <td></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+    <!-- Include Footer -->
+    <?php include($_SERVER['DOCUMENT_ROOT'] . '/en/_page_parts/footer_en.php'); ?>
+</body>
+<!--Loading bootstrap and jQuery scripts at the end of the page to prevent slowdowns-->
+<?php include($_SERVER['DOCUMENT_ROOT'] . '/src/static/pages/load_last.php'); ?>
+</script>
+
+</html>
