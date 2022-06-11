6a070e6c59277552207137c87a83f27786b7af61
Anna <djsndcx@gmail.com>
2022-06-11 18:07:57 +0200

    popup/alert mobile, full width tables
    Add popup/alert to Summary about mobile screens
    Make tables span the full width.

--- en/lineup/summary/index.php
+++ en/lineup/summary/index.php
@@ -15,9 +15,15 @@
         <div class="row justify-content-center">
             <div class="col-12 text-center">
                 <h1 class="page-header">Line-Up</h1>
+                <div class="alert alert-warning alert-dismissible fade show d-md-none" role="alert">
+                    <strong><i class="fa-solid fa-mobile-screen"></i><i class="fa-solid fa-rotate"></i></strong> Consider rotating your device to view these tables
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
             </div>
-            <div class="col-12">
-                <table class="table table-striped table-bordered table-responsive table-sm">
+            <div class="col-12 table-responsive">
+                <table class="table table-striped table-bordered table-sm">
                     <thead>
                         <tr>
                             <th scope="row" colspan="5">Thursday</th>
@@ -75,8 +81,8 @@
                     </tbody>
                 </table>
             </div>
-            <div class="col-12">
-                <table class="table table-striped table-bordered table-responsive table-sm">
+            <div class="col-12 table-responsive">
+                <table class="table table-striped table-bordered table-sm">
                     <thead>
                         <tr>
                             <th scope="row" colspan="4">Friday</th>
@@ -104,8 +110,8 @@
                     </tbody>
                 </table>
             </div>
-            <div class="col-12">
-                <table class="table table-striped table-bordered table-responsive table-sm">
+            <div class="col-12 table-responsive">
+                <table class="table table-striped table-bordered table-sm">
                     <thead>
                         <tr>
                             <th scope="row" colspan="4">Saturday</th>
