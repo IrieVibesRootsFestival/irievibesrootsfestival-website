4c801093c51da4643831b341b6e292eca6f8485a
Anna <djsndcx@gmail.com>
2022-06-11 23:42:28 +0200

    Trooper page EN, NL, FR update/translation

---
+++ fr/info/trooper/index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html><html lang="fr">
+    <head>
+        <!-- Import meta tags, OpenGraphs, stylesheets and common includes -->
+        <?php include($_SERVER['DOCUMENT_ROOT'].'/_page_parts/head_common.php');?>
+        <title>Trooper - Irie Vibes</title>
+        <meta property="og:title" content="Troop with us"/>
+        <meta property="og:description" content="Trooper peut nous aider à gagner de l'argent supplémentaire chaque fois que vous achetez dans un magasin affilié, et vous ne payez pas un centime! Travaillez-vous en troupe avec nous?" /> 
+    </head>
+    <body>
+        <?php include($_SERVER['DOCUMENT_ROOT'].'/fr/_page_parts/header_fr.php');?>
+        <!-- This is the main container that encapsulates everything content related. -->
+        <div class="container content">
+            <h1 class="page-header">Trooper</h1>
+            <div class="row">
+                <div class="col-12 col-md-8">
+                    <h3>C'est quoi Trooper?</h3>
+                    <p>Trooper est un programme d'affiliation qui nous permet de gagner de l'argent supplémentaire, alors que vous utilisez simplement le lien d'affiliation. Vous magasinez par l'intermédiaire de Trooper, et les magasins paient Trooper et Irie Vibes un pourcentage du prix total de votre commande. Vous ne payez rien de plus!</p>
+                    <p>Trop de tracas? Tu es chanceux! Trooper a construit un TrooperBot spécial. Il s'agit essentiellement d'une extension qui vous rappellera chaque fois que vous atterrirez sur une boutique en ligne affiliée à Trooper. De cette façon, vous ne manquez aucune chance d'être fier de vous pour nous avoir soutenus.</p>
+                </div>
+                <div class="col-12 col-md-4"><div class="text-center"><a href="https://drive.google.com/file/d/1b3mfn1AtoYbK0f32As_jAF5Rdo5DswtU/view"><img alt="" class="img-fluid rounded mb-2" src="https://www.trooper.be/Content/Assets/logo_trooper_small.gif"></a></div></div>
+                <div class="col-12">
+                    <h3>Comment installer TrooperBot?</h3>
+                    <p>TrooperBot s'installe facilement sur Chrome et Firefox. En cliquant sur le logo Trooper ci-dessus, vous accéderez au guide officiel de Trooper sur l'installation de TrooperBot (mais ils n'ont pas d'hyperliens!) Pour les utilisateurs d'Opera, veuillez installer ce plugin de compatibilité par l'équipe Opera et suivez les instructions pour les utilisateurs de Chrome. Voici comment installer TrooperBot sur votre navigateur:</p>
+                    <ol>
+                        <li>Accédez à <a href="www.trooper.be/trooperbot">www.trooper.be/trooperbot</a></li>
+                        <li>Cliquez sur l'icône de votre navigateur correspondant. <i>Pour les utilisateurs d'Opera, voir l'avis ci-dessus.</i><br/>
+                        <li>Cliquez sur "Ajouter à Firefox" ou "Ajouter à Chrome"</li>
+                        <li>Ajoutez <b>VZW GreenForward</b>: TrooperBot vous demandera de leur dire qui vous êtes et quelle organisation vous souhaitez soutenir. Remplissez votre nom ou d'autres informations (si vous souhaitez rester anonyme) et ajoutez Rechercher et ajouter <b>VZW GreenForward</b> comme l'organisation que vous souhaitez soutenir.</li>
+                        <li>Vous avez terminé! N'oubliez pas de faire attention au rappel de TrooperBot chaque fois que vous visitez un site affilié!<br/>
+                    </ol>
+                </div>
+                <div class="col-12 col-md-8">
+                    <h3>Alternatively...</h3>
+                    <p>Vous pouvez également nous soutenir en cliquant sur la boutique en ligne de votre choix sur <a href="https://www.trooper.be/nl/trooperverenigingen/greenforward">cette page</a>. Trooper ajoutera automatiquement les informations nécessaires au lien Web.</p>
+                </div>
+            </div>
+        </div>
+        <!-- Include Footer -->
+        <?php include($_SERVER['DOCUMENT_ROOT'].'/fr/_page_parts/footer_fr.php');?>        
+    </body>
+    <!--Loading bootstrap and jQuery scripts at the end of the page to prevent slowdowns-->
+    <?php include($_SERVER['DOCUMENT_ROOT'].'/_page_parts/load_last.php');?>
+</html>
