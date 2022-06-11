03c72247b92b0197bd3d609d384d7914b454e066
Anna <djsndcx@gmail.com>
2022-06-11 02:34:15 +0200

    Load Last relocation

--- fr/festival/safety/index.php
+++ fr/festival/safety/index.php
@@ -28,5 +28,5 @@
         <?php include($_SERVER['DOCUMENT_ROOT'].'/fr/_page_parts/footer_fr.php');?>        
     </body>
     <!--Loading bootstrap and jQuery scripts at the end of the page to prevent slowdowns-->
-    <?php include($_SERVER['DOCUMENT_ROOT'].'/src/static/pages/load_last.php');?>
+    <?php include($_SERVER['DOCUMENT_ROOT'].'_page_parts/load_last.php');?>
 </html>
