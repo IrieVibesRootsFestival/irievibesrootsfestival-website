a5fba9a453af7d9226a1441e1004ce9c0f4ce320
Anna <djsndcx@gmail.com>
2022-06-17 19:55:34 +0200

    Change k conge dates (#68)
    * Create production.yml
    Update production.yml
    
    * Emergency care for Kortemark Conge (dates & more)
    Extra deco / layout / comments
    Clean up & correct Kortemark Conge pages

--- fr/festival/kortemark-conge/index.php
+++ fr/festival/kortemark-conge/index.php
@@ -1,173 +1,139 @@
 <!DOCTYPE html>
 <html lang="fr">
-    <head>
-        <!-- Import meta tags, OpenGraphs, stylesheets and common includes -->
-        <?php include($_SERVER['DOCUMENT_ROOT'].'/_page_parts/head_common.php');?>
-        <title>Kortemark Congé - Irie Vibes</title>
-        <meta property="og:title" content="Kortemark Congé"/>
-        <meta property="og:description" content="Tous ce que tu veux savoir sur Kortemark Congé" />
-    </head>
-    <body>
-        <?php include($_SERVER['DOCUMENT_ROOT'].'/fr/_page_parts/header_fr.php');?>
-        <!-- Main Content --> 
-        <div class="container content">
-            <h1 class="page-header">Kortemark Conge</h1>
-            <div class="row"> 
-                <!-- Text block block (left) -->
-                <div class="col-12">
-                    <h3 class="text-irie text-uppercase">22 juillet 2022 à partir de 18u00</h3>
-                    <a href="https://www.facebook.com/events/2326818524093825/">Kortemark Congé 2022 - Facebook Events</a>
-                    <p>vzw Greenforward vous souhaite la bienvenue à notre soirée festival GRATUITE “Kortemark Congé”</p>
-                    <ul>
-                        <li>
-                            <h3 class="text-success"><b>Lion’s Den: “a touch of reggae, a lot of vibes”</b></h3>
-                            <p>
-                                Le chaleur, l’hospitalité, les rythmes et vibes 
-                            </p>
-                            <div class="row m-0 p-0">
-                                <!--KConge 2018-->
-                                <div class="col-12 col-md-4">
-                                    <div class="embed-responsive embed-responsive-16by9 rounded mb-3">
-                                        <iframe class="embed-responsive-item" src="https://www.youtube-nocookie.com/embed/6GhMiDClsqk" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                                    </div>
-                                </div>
-                                <!--KConge 2019-->
-                                <div class="col-12 col-md-4">
-                                    <div class="embed-responsive embed-responsive-16by9 rounded mb-3">
-                                        <iframe class="embed-responsive-item" src="https://www.youtube-nocookie.com/embed/uaY9Y4iB5nE" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                                    </div>
-                                </div>
-                                <!--Focus WTV Clip-->
-                                <div class="col-12 col-md-4">
-                                    <div class="embed-responsive embed-responsive-16by9 rounded mb-3">
-                                        <iframe class="embed-responsive-item" src="//player.cdn01.rambla.be?account_id=VzaPKg&amp;item_id=W9MoDP" frameborder="0"></iframe>
-                                    </div>
-                                </div>
-                            </div>
-                        </li>
-                        <li>
 
-                            <h3 class="text-warning"><b>Trim the footprint: “because there is no planet B” </b></h3>
-                            <ul>
-                                <li>ateliers et conférences</li>
-                                <li>avec le support d’Interreg France-Wallonie-Vlaanderen</li>
-                                <li>les initiatives écologiques à l’honneur</li>
-                            </ul>
-                            <div class="row m-0 p-0">
-                                <div class="col-12 col-md-4">
-                                    <div class="embed-responsive embed-responsive-16by9 rounded mb-3">
-                                        <iframe class="embed-responsive-item" src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2FIrieVibesRootsFesitval%2Fvideos%2F416779585895356%2F&show_text=0&width=560" width="560" height="315" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allowFullScreen="true"></iframe>
-                                    </div>
-                                </div>
-                                <div class="col-12 col-md-4 text-center">
-                                    <img alt="" width="60%" src="/src/img/eco/trim-the-footprint/trimthefootprint.png" class="img-fluid rounded"/>
-                                </div>
-                                <div class="col-12 col-md-4">
-                                    <picture>
-                                        <source srcset="/src/img/partners/interreg.webp" type="image/webp">
-                                        <source srcset="/src/img/partners/interreg.jpg" type="image/jpeg">
-                                        <img alt="" class="img-fluid rounded" src="/src/img/partners/interreg.jpg"/>
-                                    </picture>
-                                </div>
-                            </div>
-                        </li>
-                        <li>
-                            <h3 class="text-danger"><b>Wereldmarkt: “inspire the world”</b></h3>
-                            <p>
-                                <i>Kortemark n’est pas d’une autre monde</i>
-                            </p>
-                            <ul>
-                                <li>foodtrucks / world food / shops</li>
-                                <li>marché mondial : associations et projets locaux</li>
-                                <li>objectifs de développement</li>
-                            </ul>
-                            <p>
-                                <br/>avec Noord Zuid Kortemark en Weedo Tanzania<br/> <a href="https://www.facebook.com/weedo.womenempowering/">https://www.facebook.com/weedo.womenempowering/</a>
+<head>
+    <!-- Import meta tags, OpenGraphs, stylesheets and common includes -->
+    <?php include($_SERVER['DOCUMENT_ROOT'] . '/src/page_parts/head_common.php'); ?>
+    <title>Kortemark Congé - Irie Vibes</title>
+    <meta property="og:title" content="Kortemark Congé" />
+    <meta property="og:description" content="Tous ce que tu veux savoir sur Kortemark Congé" />
+</head>
 
-                            </p>
-                            <div class="row">
-                                <!-- Begin pictures -->
-
-                                <div class="col-6 col-md-4 col-xl-3 pb-1">
-                                    <picture>
-                                        <source srcset="/src/img/eco/sdg/intro/color-circle.webp" type="image/webp">
-                                        <source srcset="/src/img/eco/sdg/intro/color-circle.jpg" type="image/jpeg">
-                                        <img alt="" class="img-fluid rounded" src="/src/img/eco/sdg/intro/color-circle.jpg"/>
-                                    </picture>
-                                </div>
-                                <div class="col-6 col-md-4 col-xl-3 pb-1">
-                                    <picture>
-                                        <source srcset="/src/img/eco/sdg/intro/intro-square.webp" type="image/webp">
-                                        <source srcset="/src/img/eco/sdg/intro/intro-square.jpg" type="image/jpeg">
-                                        <img alt="" class="img-fluid rounded" src="/src/img/eco/sdg/intro/intro-square.jpg"/>
-                                    </picture>
-                                </div>
-                                <div class="col-6 col-md-4 col-xl-3 pb-1">
-                                    <picture>
-                                        <source srcset="/src/img/eco/sdg/intro/SDG.webp" type="image/webp">
-                                        <source srcset="/src/img/eco/sdg/intro/SDG.jpg" type="image/jpeg">
-                                        <img alt="" class="img-fluid rounded" src="/src/img/eco/sdg/intro/SDG.jpg"/>
-                                    </picture>
-                                </div>
-                                <div class="col-6 col-md-4 col-xl-3 pb-1">
-                                    <picture>
-                                        <source srcset="/src/img/eco/sdg/intro/world-in-hands.webp" type="image/webp">
-                                        <source srcset="/src/img/eco/sdg/intro/world-in-hands.jpg" type="image/jpeg">
-                                        <img alt="" class="img-fluid rounded" src="/src/img/eco/sdg/intro/world-in-hands.jpg"/>
-                                    </picture>
-                                </div>
-                            </div>
-                            <!-- End Pictures -->
-                        </li>
-                        <li>
-                            <h3 class="text-dark"><b>Little Lion: “children see magic because they look for it”</b></h3>
-                            <ul>
-                                <li>Recycle art</li>
-                                <li>Village d’enfants</li>
-                                <li>Coiffures folles</li>
-                                <li>Qi Gong</li>
-                                <li>Workshops</li>
-                                <li>Construire un monde meilleur</li>
-                            </ul>
-                        </li>
-                    </ul>
-                </div>
-                <div class="col-12"><h3 class="text-irie text-uppercase">Flyer Kortemark Congé - Irie Vibes Roots Festival 2019</h3></div>
-                <!-- Two poster halves -->
-                <div class="col-12 col-md-6">
-                    <picture>
-                        <source srcset="/src/img/kortemark-conge/kortemarkconge-flyer-front.webp" type="image/webp">
-                        <source srcset="/src/img/kortemark-conge/kortemarkconge-flyer-front.jpg" type="image/jpeg">
-                        <img alt="" class="img-fluid rounded" src="/src/img/kortemark-conge/kortemarkconge-flyer-front.jpg"/>
-                    </picture>
-                </div>
-                <div class="col-12 col-md-6">
-                    <picture>
-                        <source srcset="/src/img/kortemark-conge/kortemarkconge-flyer-back.webp" type="image/webp">
-                        <source srcset="/src/img/kortemark-conge/kortemarkconge-flyer-back.jpg" type="image/jpeg">
-                        <img alt="" class="img-fluid rounded" src="/src/img/kortemark-conge/kortemarkconge-flyer-back.jpg"/>
-                    </picture>
-                </div>
-                <!-- Hide line-up on mobile, and display a link to the Line-Up page instead -->
-                <div class="col-12 d-none d-md-block mt-md-2">
-                    <!-- Include page that holds images/links -->
-                    <?php include($_SERVER['DOCUMENT_ROOT'].'/en/lineup/kortemark-conge/core_lineup.php');?>
-                </div>
-                <div class="col-12 d-block d-md-none text-center">
-                    <button class="btn btn-pill btn-irie"><a class="text-white" href="/en/lineup/kortemark-conge/">View Line-up</a></button>
-                </div>
+<body>
+    <?php include($_SERVER['DOCUMENT_ROOT'] . '/fr/_page_parts/header_fr.php'); ?>
+    <!-- Main Content -->
+    <div class="container content">
+        <h1 class="page-header">Kortemark Conge</h1>
+        <div class="row">
+            <!-- Text block block (left) -->
+            <div class="col-12">
+                <h3 class="text-irie text-uppercase"><b>Jeudi 21 juillet 2022</b> à partir de 18u00</h3>
+                <a href="https://www.facebook.com/events/2326818524093825/">Kortemark Congé 2022 - Facebook Events</a>
+                <p>vzw Greenforward vous souhaite la bienvenue à notre soirée festival GRATUITE “Kortemark Congé”</p>
             </div>
-        </div>        
-        <!-- Include Footer -->
-        <?php include($_SERVER['DOCUMENT_ROOT'].'/fr/_page_parts/footer_fr.php');?>        
-    </body>
-    <!--Loading bootstrap and jQuery scripts at the end of the page to prevent slowdowns-->
-    <?php include($_SERVER['DOCUMENT_ROOT'].'/_page_parts/load_last.php');?>
-    <div id="fb-root"></div>
-    <script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v3.2"></script>
-    <!-- Initialize Tooltips--><script>
-    $(function () {
-        $('[data-toggle="popover"]').popover()
-    })
-    </script>
+            <div class="col-12"><!-- Lion's Den --> 
+                <h3 class="text-success"><b>Lion's Den: “a touch of reggae, a lot of vibes”</b></h3>
+                <p>
+                    Le chaleur, l'hospitalité, les rythmes et vibes
+                </p>
+                <div class="row m-0 p-0">
+                    <!--KConge 2018-->
+                    <div class="col-12 col-md-4">
+                        <div class="embed-responsive embed-responsive-16by9 rounded mb-3">
+                            <iframe class="embed-responsive-item" src="https://www.youtube-nocookie.com/embed/6GhMiDClsqk" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                        </div>
+                    </div>
+                    <!--KConge 2019-->
+                    <div class="col-12 col-md-4">
+                        <div class="embed-responsive embed-responsive-16by9 rounded mb-3">
+                            <iframe class="embed-responsive-item" src="https://www.youtube-nocookie.com/embed/uaY9Y4iB5nE" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                        </div>
+                    </div>
+                    <!--Focus WTV Clip-->
+                    <div class="col-12 col-md-4">
+                        <div class="embed-responsive embed-responsive-16by9 rounded mb-3">
+                            <iframe class="embed-responsive-item" src="//player.cdn01.rambla.be?account_id=VzaPKg&amp;item_id=W9MoDP" frameborder="0"></iframe>
+                        </div>
+                    </div>
+                </div>
+            </div><!-- END Lion's Den -->
+            <div class="col-12"><!-- Trim The Footprint --> 
+                <h3 class="text-warning"><b>Trim The Footprint: “because there is no planet B”</b></h3>
+                <ul>
+                    <li>ateliers et conférences</li>
+                    <li>avec le support d'Interreg France-Wallonie-Vlaanderen</li>
+                    <li>les initiatives écologiques à l'honneur</li>
+                </ul>
+                <div class="row m-0 p-0">
+                    <div class="col-12 col-md-4">
+                        <div class="embed-responsive embed-responsive-16by9 rounded mb-3">
+                            <iframe class="embed-responsive-item" src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2FIrieVibesRootsFesitval%2Fvideos%2F416779585895356%2F&show_text=0&width=560" width="560" height="315" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allowFullScreen="true"></iframe>
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-4 text-center">
+                        <img alt="" width="60%" src="/src/img/eco/trim-the-footprint/trimthefootprint.png" class="img-fluid rounded" />
+                    </div>
+                    <div class="col-12 col-md-4">
+                        <picture>
+                            <source srcset="/src/img/partners/interreg.webp" type="image/webp">
+                            <source srcset="/src/img/partners/interreg.jpg" type="image/jpeg">
+                            <img alt="" class="img-fluid rounded" src="/src/img/partners/interreg.jpg" />
+                        </picture>
+                    </div>
+                </div>
+            </div><!-- END Trim The Footprint --> 
+            <div class="col-12"><!-- World Markets --> 
+                <h3 class="text-danger"><b>Marché Mondial: “inspire the world”</b></h3>
+                <p>
+                    <i>Kortemark n'est pas d'une autre monde</i>
+                </p>
+                <ul>
+                    <li>food trucks / world food / shops</li>
+                    <li>Marché Mondial: groupes Noord-Zuid et des associations et projets locaux</li>
+                    <li>Objectifs de Développement (SDGs)</li>
+                </ul>
+                <p>
+                    En coop avec Noord-Zuid Kortemark et Weedo Tanzania<br/>
+                    Trouvez Weedo Tanzania en <a href="https://www.facebook.com/weedo.womenempowering/">Facebook!</a>
+                </p>
+                <div class="row"><!-- Begin pictures -->                    
+                    <div class="col-6 col-md-4 col-xl-3 pb-1">
+                        <picture>
+                            <source srcset="/src/img/eco/sdg/intro/color-circle.webp" type="image/webp">
+                            <source srcset="/src/img/eco/sdg/intro/color-circle.jpg" type="image/jpeg">
+                            <img alt="" class="img-fluid rounded" src="/src/img/eco/sdg/intro/color-circle.jpg" />
+                        </picture>
+                    </div>
+                    <div class="col-6 col-md-4 col-xl-3 pb-1">
+                        <picture>
+                            <source srcset="/src/img/eco/sdg/intro/intro-square.webp" type="image/webp">
+                            <source srcset="/src/img/eco/sdg/intro/intro-square.jpg" type="image/jpeg">
+                            <img alt="" class="img-fluid rounded" src="/src/img/eco/sdg/intro/intro-square.jpg" />
+                        </picture>
+                    </div>
+                    <div class="col-6 col-md-4 col-xl-3 pb-1">
+                        <picture>
+                            <source srcset="/src/img/eco/sdg/intro/SDG.webp" type="image/webp">
+                            <source srcset="/src/img/eco/sdg/intro/SDG.jpg" type="image/jpeg">
+                            <img alt="" class="img-fluid rounded" src="/src/img/eco/sdg/intro/SDG.jpg" />
+                        </picture>
+                    </div>
+                    <div class="col-6 col-md-4 col-xl-3 pb-1">
+                        <picture>
+                            <source srcset="/src/img/eco/sdg/intro/world-in-hands.webp" type="image/webp">
+                            <source srcset="/src/img/eco/sdg/intro/world-in-hands.jpg" type="image/jpeg">
+                            <img alt="" class="img-fluid rounded" src="/src/img/eco/sdg/intro/world-in-hands.jpg" />
+                        </picture>
+                    </div>
+                </div><!-- END Pictures -->               
+            </div><!-- END World Markets --> 
+            <div class="col-12"><!-- Little Lion --> 
+                <h3 class="text-dark"><b>Little Lion: “children see magic because they look for it”</b></h3>
+                <ul>
+                    <li>Recycle art</li>
+                    <li>Village d'enfants</li>
+                    <li>Coiffures folles</li>
+                    <li>Qi Gong</li>
+                    <li>Workshops</li>
+                    <li>Construire un monde meilleur</li>
+                </ul>
+            </div><!-- END Little Lion -->
+        </div>
+    </div>
+    <!-- Include Footer -->
+    <?php include($_SERVER['DOCUMENT_ROOT'] . '/src/static/pages/footer_fr.php'); ?>
+</body>
+<!--Loading bootstrap and jQuery scripts at the end of the page to prevent slowdowns-->
+<?php include($_SERVER['DOCUMENT_ROOT'] . '/src/static/pages/load_last.php'); ?>
+
 </html>
