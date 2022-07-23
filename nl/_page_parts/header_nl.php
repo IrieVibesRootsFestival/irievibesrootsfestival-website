5a5b512392f912c5fc5b6c38c7b4c1a186caabf4
Anna <djsndcx@gmail.com>
2022-07-23 13:52:00 +0200

    Header nav changes for Ticket price page

--- nl/_page_parts/header_nl.php
+++ nl/_page_parts/header_nl.php
@@ -57,7 +57,7 @@
                         <a class="nav-link" href="/nl/lineup/summary/"><i class="fas fa-record-vinyl"></i>&nbsp;Line-up</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link text-danger" href="https://tickets.irievibes.be"><i class="fas fa-ticket"></i>&nbsp;Tickets</a>
+                        <a class="nav-link text-danger" href="/nl/tickets"><i class="fas fa-ticket"></i>&nbsp;Tickets</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="/nl/kids/">Little Lion</a>
